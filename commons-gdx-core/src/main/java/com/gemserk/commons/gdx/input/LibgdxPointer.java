03190ac8dd5cbd8bd6d790f1ed591d24f9e040df
Ariel Coppes <ariel.coppes@gemserk.com>
2011-04-12 11:10:59 -0300

    added a BooleanValue and fixed a bug in the LibgdxPointer class

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/input/LibgdxPointer.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/input/LibgdxPointer.java
@@ -1,7 +1,9 @@
 package com.gemserk.commons.gdx.input;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 
 public class LibgdxPointer {
 
@@ -9,41 +11,81 @@ public class LibgdxPointer {
 
 	Vector2 pressedPosition = new Vector2();
 
+	Vector3 pressedPosition3d = new Vector3();
+
 	Vector2 releasedPosition = new Vector2();
+
+	Vector3 releasedPosition3d = new Vector3();
 
 	public boolean wasPressed;
 
 	public boolean wasReleased;
 
 	public int index;
+
+	private OrthographicCamera camera;
 	
+	public Vector2 getPressedPosition() {
+		return pressedPosition;
+	}
+	
+	public Vector2 getReleasedPosition() {
+		return releasedPosition;
+	}
+
 	public LibgdxPointer(int index) {
+		this(index, null);
+	}
+
+	public LibgdxPointer(int index, OrthographicCamera camera) {
 		this.index = index;
+		this.camera = camera;
 	}
 
 	public void update() {
-		
+
 		if (Gdx.input.isTouched(index)) {
-			
+
 			if (!touched) {
 				touched = true;
 				wasPressed = true;
-				pressedPosition.set(Gdx.input.getX(index), Gdx.graphics.getHeight() - Gdx.input.getY(index));
-			} else 
+				
+				if (camera != null) {
+
+					pressedPosition3d.set(Gdx.input.getX(index), Gdx.input.getY(index), 0f);
+					camera.unproject(pressedPosition3d);
+					pressedPosition.set(pressedPosition3d.x, pressedPosition3d.y);
+
+				} else {
+					pressedPosition.set(Gdx.input.getX(index), Gdx.graphics.getHeight() - Gdx.input.getY(index));
+				}
+
+			} else {
 				wasPressed = false;
-			
-		} 
-		
-		if (Gdx.input.isTouched(index)) {
-			
+			}
+
+		}
+
+		if (!Gdx.input.isTouched(index)) {
+
 			if (touched) {
 				touched = false;
 				wasReleased = true;
-				releasedPosition.set(Gdx.input.getX(index), Gdx.graphics.getHeight() - Gdx.input.getY(index));
+				
+				if (camera != null) {
+					
+					releasedPosition3d.set(Gdx.input.getX(index), Gdx.input.getY(index), 0f);
+					camera.unproject(releasedPosition3d);
+					releasedPosition.set(releasedPosition3d.x, releasedPosition3d.y);
+
+				} else {
+					releasedPosition.set(Gdx.input.getX(index), Gdx.graphics.getHeight() - Gdx.input.getY(index));
+				}
+
 			} else {
 				wasReleased = false;
 			}
-			
+
 		}
 	}
 
---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/values/BooleanValue.java
@@ -0,0 +1,11 @@
+package com.gemserk.commons.values;
+
+public class BooleanValue {
+
+	public boolean value = false;
+
+	public BooleanValue(boolean value) {
+		this.value = value;
+	}
+
+}
