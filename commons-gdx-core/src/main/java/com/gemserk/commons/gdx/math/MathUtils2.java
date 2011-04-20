72b42e33dfaabc4a42903268d4e6522ec2ac92ae
Ariel Coppes <ariel.coppes@gemserk.com>
2011-04-20 13:01:59 -0300

    added some methods to make common math tasks

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/math/MathUtils2.java
@@ -0,0 +1,29 @@
+package com.gemserk.commons.gdx.math;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class MathUtils2 {
+
+	/**
+	 * Returns true if the point is inside the rectangle, false otherwise
+	 */
+	public static boolean inside(Rectangle r, Vector2 p) {
+
+		if (p.x < r.x)
+			return false;
+
+		if (p.y < r.y)
+			return false;
+
+		if (p.x > r.x + r.width)
+			return false;
+
+		if (p.y > r.y + r.height)
+			return false;
+
+		return true;
+
+	}
+
+}
