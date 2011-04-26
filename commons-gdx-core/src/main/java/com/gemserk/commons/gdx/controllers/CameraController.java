c6b70cd9b7d27b1bb16a14af72c4ceec93228413
Ariel Coppes <ariel.coppes@gemserk.com>
2011-04-26 18:48:28 -0300

    added CameraController interface

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/controllers/CameraController.java
@@ -0,0 +1,10 @@
+package com.gemserk.commons.gdx.controllers;
+
+import com.gemserk.commons.gdx.camera.Camera;
+import com.gemserk.commons.gdx.controllers.Controller;
+
+public interface CameraController extends Controller {
+
+	Camera getCamera();
+
+}
