567350905ba830926f802d0dd819607f878e3ad9
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-01 11:33:33 -0300

    removed constructor and changed internal impl

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/camera/Camera.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/camera/Camera.java
@@ -1,58 +1,51 @@
 package com.gemserk.commons.gdx.camera;
 
-import com.badlogic.gdx.math.Vector2;
-
 public class Camera {
-	
-	private final Vector2 position = new Vector2();
-	
+
+	private float x, y;
+
 	private float zoom;
-	
+
 	private float angle;
-	
+
 	public Camera() {
 		this(0f, 0f, 1f, 0f);
 	}
 
-	public Camera(Vector2 position, float zoom, float angle) {
-		this.position.set(position);
-		this.zoom = zoom;
-		this.angle = angle;
-	}
-	
 	public Camera(float x, float y, float zoom, float angle) {
-		this.position.set(x, y);
+		this.x = x;
+		this.y = y;
 		this.zoom = zoom;
 		this.angle = angle;
 	}
-	
+
 	public void setPosition(float x, float y) {
-		position.set(x,y);
+		this.x = x;
+		this.y = y;
 	}
-	
+
 	public void setZoom(float zoom) {
 		this.zoom = zoom;
 	}
-	
+
 	public void setAngle(float angle) {
 		this.angle = angle;
 	}
-	
+
 	public float getZoom() {
 		return zoom;
 	}
-	
+
 	public float getAngle() {
 		return angle;
 	}
-	
-	public float getX() { 
-		return position.x;
+
+	public float getX() {
+		return x;
 	}
-	
+
 	public float getY() {
-		return position.y;
+		return y;
 	}
-	
-	
+
 }
