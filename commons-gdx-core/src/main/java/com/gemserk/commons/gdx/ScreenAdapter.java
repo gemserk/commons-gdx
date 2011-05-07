83ce1559145c6f008bce12f6c9a42ff3c322aac3
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-07 14:59:44 -0300

    added internalRender and internalUpdate methods to be overriden by childs of ScreenAdapter

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/ScreenAdapter.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/ScreenAdapter.java
@@ -4,39 +4,51 @@ import com.badlogic.gdx.Screen;
 
 public class ScreenAdapter implements Screen {
 
+	/**
+	 * Don't override if you want to use internalUpdate or internalRender separated methods.
+	 */
 	@Override
 	public void render(float delta) {
-		
+		internalRender(delta);
+		internalUpdate(delta);
+	}
+
+	public void internalRender(float delta) {
+
+	}
+
+	public void internalUpdate(float delta) {
+
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		
+
 	}
 
 	@Override
 	public void show() {
-		
+
 	}
 
 	@Override
 	public void hide() {
-		
+
 	}
 
 	@Override
 	public void pause() {
-		
+
 	}
 
 	@Override
 	public void resume() {
-		
+
 	}
 
 	@Override
 	public void dispose() {
-		
+
 	}
 
 }
