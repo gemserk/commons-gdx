91c0b90e7514d7610b980b7773d2c14c486781f4
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-09 13:25:54 -0300

    added method to render a Sprite centered

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/graphics/SpriteBatchUtils.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/graphics/SpriteBatchUtils.java
@@ -2,6 +2,7 @@ package com.gemserk.commons.gdx.graphics;
 
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SpriteBatchUtils {
@@ -14,4 +15,15 @@ public class SpriteBatchUtils {
 		font.draw(spriteBatch, text, x - bounds.width * 0.5f, y + bounds.height * 0.5f);
 	}
 
+	
+	/**
+	 * Draws a Sprite centered.
+	 */
+	public static void drawCentered(SpriteBatch spriteBatch, Sprite sprite, float x, float y, float w, float h, float angle) {
+		sprite.setSize(w, h);
+		sprite.setOrigin(w * 0.5f, h * 0.5f);
+		sprite.setPosition(x - sprite.getOriginX(), y - sprite.getOriginY());
+		sprite.setRotation(angle);
+		sprite.draw(spriteBatch);
+	}
 }
