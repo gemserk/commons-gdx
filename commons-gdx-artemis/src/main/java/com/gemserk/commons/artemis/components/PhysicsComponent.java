53217a49887d660728c0259c24f87f56b9e057b8
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-13 14:25:45 -0300

    modified PhysicsComponent to avoid using Property<> internally

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/PhysicsComponent.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/PhysicsComponent.java
@@ -2,38 +2,24 @@ package com.gemserk.commons.artemis.components;
 
 import com.artemis.Component;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.gemserk.componentsengine.properties.Property;
-import com.gemserk.componentsengine.properties.SimpleProperty;
 
 public class PhysicsComponent extends Component {
 
-	private Property<Body> body;
+	private Body body;
 	
-	private Property<Contact> contact;
+	private Contact contact;
 	
 	public Body getBody() {
-		return body.get();
+		return body;
 	}
 	
 	public Contact getContact() {
-		return contact.get();
-	}
-	
-	public void setContact(Contact contact) {
-		this.contact.set(contact);
+		return contact;
 	}
 	
 	public PhysicsComponent(Body body) {
-		this(new SimpleProperty<Body>(body));
-	}
-	
-	public PhysicsComponent(Property<Body> body) {
-		this(body, new SimpleProperty<Contact>(new Contact()));
-	}
-	
-	public PhysicsComponent(Property<Body> body, Property<Contact> contact) {
 		this.body = body;
-		this.contact = contact;
+		this.contact = new Contact();
 	}
 
 }
