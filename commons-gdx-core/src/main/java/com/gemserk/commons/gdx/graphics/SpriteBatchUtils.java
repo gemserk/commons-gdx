e1d7ef9e04d60e0a3c779b547476fb6f27b53864
Ariel Coppes <ariel.coppes@gemserk.com>
2011-05-24 19:34:32 -0300

    added Text class to hold a Text in the screen logic modified SpriteBatchUtils to accept centerx, centery values when drawing a multiline text

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/graphics/SpriteBatchUtils.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/graphics/SpriteBatchUtils.java
@@ -8,6 +8,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class SpriteBatchUtils {
 
 	/**
+	 * Used to define how to center the text box over the (x,y) coordinates, not for internal text alignment, for that BitmapFont has HAlignment.
+	 */
+	public static enum Align {
+		Center, Left, Right
+	};
+
+	/**
 	 * Draws a text centered in the specified coordinates
 	 */
 	public static void drawCentered(SpriteBatch spriteBatch, BitmapFont font, String text, float x, float y) {
@@ -19,8 +26,25 @@ public class SpriteBatchUtils {
 	 * Draws a multi line text centered.
 	 */
 	public static void drawMultilineTextCentered(SpriteBatch spriteBatch, BitmapFont font, String text, float x, float y) {
+		drawMultilineText(spriteBatch, font, text, x, y, 0.5f, 0.5f);
+	}
+
+	/**
+	 * Draws a multi line text in the specified coordinates (x,y). It will use cx and cy to center the text over the coordinates (x,y).
+	 * 
+	 * @param spriteBatch
+	 * @param font
+	 * @param text
+	 * @param x
+	 * @param y
+	 * @param cx
+	 *            A value between 0 and 1 to center the text over the horizontal axis.
+	 * @param cy
+	 *            A value between 0 and 1 to center the text over the vertical axis.
+	 */
+	public static void drawMultilineText(SpriteBatch spriteBatch, BitmapFont font, String text, float x, float y, float cx, float cy) {
 		TextBounds bounds = font.getMultiLineBounds(text);
-		font.drawMultiLine(spriteBatch, text, x - bounds.width * 0.5f, y + bounds.height * 0.5f);
+		font.drawMultiLine(spriteBatch, text, x + bounds.width * cx, y + bounds.height * cy);
 	}
 
 	/**
---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/gui/Text.java
@@ -0,0 +1,66 @@
+package com.gemserk.commons.gdx.gui;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.gemserk.commons.gdx.graphics.SpriteBatchUtils;
+
+public class Text {
+
+	private float x;
+
+	private float y;
+
+	private String text;
+
+	private float cx, cy;
+
+	private boolean visible;
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	/**
+	 * Used to center the text over (x,y) coordinates.
+	 * 
+	 * @param cx
+	 *            A value between 0 and 1 to center over horizontal axis.
+	 * @param cy
+	 *            A value between 0 and 1 to center over vertical axis.
+	 */
+	public void setCenter(float cx, float cy) {
+		this.cx = cx;
+		this.cy = cy;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public Text(String text, float x, float y) {
+		this(text, x, y, 0.5f, 0.5f);
+	}
+
+	public Text(String text, float x, float y, float cx, float cy) {
+		this.text = text;
+		this.x = x;
+		this.y = y;
+		this.cx = cx;
+		this.cy = cy;
+	}
+
+	public void draw(SpriteBatch spriteBatch, BitmapFont font) {
+		if (!isVisible())
+			return;
+		SpriteBatchUtils.drawMultilineText(spriteBatch, font, text, x, y, cx, cy);
+	}
+
+}
