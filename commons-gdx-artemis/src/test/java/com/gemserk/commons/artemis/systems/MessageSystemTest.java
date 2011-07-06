7ffeb4e141e96ca6457b2d9253f14a3b8fbf764b
Ariel Coppes <ariel.coppes@gemserk.com>
2011-07-06 13:42:35 -0300

    added some ideas about a message system using artemis

---
+++ commons-gdx-artemis/src/test/java/com/gemserk/commons/artemis/systems/MessageSystemTest.java
@@ -0,0 +1,120 @@
+package com.gemserk.commons.artemis.systems;
+
+import java.util.ArrayList;
+
+import com.artemis.Component;
+import com.artemis.Entity;
+import com.artemis.EntityProcessingSystem;
+
+public class MessageSystemTest {
+
+	class Message {
+
+		private final Entity source;
+		private final String id;
+
+		public String getId() {
+			return id;
+		}
+
+		public Entity getSource() {
+			return source;
+		}
+
+		public Message(String id, Entity source) {
+			this.id = id;
+			this.source = source;
+		}
+
+	}
+
+	class MessageQueueComponent extends Component {
+
+		private ArrayList<Message> messages;
+
+		public ArrayList<Message> getMessages() {
+			return messages;
+		}
+
+	}
+
+	class MessageQueue {
+
+		private ArrayList<Message> messages;
+
+		public ArrayList<Message> getMessages() {
+			return messages;
+		}
+
+	}
+
+	class MessageQueueSystem extends EntityProcessingSystem {
+
+		private MessageQueue messageQueue;
+
+		public void setMessageQueue(MessageQueue messageQueue) {
+			this.messageQueue = messageQueue;
+		}
+
+		public MessageQueueSystem() {
+			super(MessageQueueComponent.class);
+		}
+
+		@Override
+		protected void process(Entity e) {
+			MessageQueueComponent messageQueueComponent = e.getComponent(MessageQueueComponent.class);
+			messageQueue.getMessages().addAll(messageQueueComponent.getMessages());
+			messageQueueComponent.getMessages().clear();
+		}
+
+	}
+
+	class MessageHandler {
+
+		boolean canHandle(Message m) {
+			return true;
+		}
+
+		void handle(Message m) {
+
+		}
+
+	}
+
+	class MessageHandlerComponent extends Component {
+
+		ArrayList<MessageHandler> messageHandlers;
+
+	}
+
+	class MessageListenerSystem extends EntityProcessingSystem {
+
+		private MessageQueue messageQueue;
+
+		public void setMessageQueue(MessageQueue messageQueue) {
+			this.messageQueue = messageQueue;
+		}
+
+		public MessageListenerSystem() {
+			super(MessageHandlerComponent.class);
+		}
+
+		@Override
+		protected void process(Entity e) {
+			MessageHandlerComponent messageHandlerComponent = e.getComponent(MessageHandlerComponent.class);
+			ArrayList<MessageHandler> messageHandlers = messageHandlerComponent.messageHandlers;
+			for (int i = 0; i < messageHandlers.size(); i++) {
+				MessageHandler messageHandler = messageHandlers.get(i);
+				// if (messageHandler.canHandle(m))
+				for (int j = 0; j < messageQueue.messages.size(); j++) {
+					Message m = messageQueue.messages.get(j);
+					if (messageHandler.canHandle(m))
+						messageHandler.handle(m);
+				}
+			}
+			messageQueue.messages.clear();
+		}
+
+	}
+
+}
