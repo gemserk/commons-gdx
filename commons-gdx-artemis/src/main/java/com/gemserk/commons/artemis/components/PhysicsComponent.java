7369193bb3efcd2be862c91cfcc79592f7050458
Ariel Coppes <ariel.coppes@gemserk.com>
2011-07-12 12:22:15 -0300

    modified PhysicsComponent to use a Physics class internally

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/PhysicsComponent.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/PhysicsComponent.java
@@ -3,24 +3,31 @@ package com.gemserk.commons.artemis.components;
 import com.artemis.Component;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.gemserk.commons.gdx.box2d.Contact;
+import com.gemserk.commons.gdx.games.Physics;
+import com.gemserk.commons.gdx.games.PhysicsImpl;
 
 public class PhysicsComponent extends Component {
 
-	private Body body;
-	
-	private Contact contact;
+	private final Physics physics;
 	
 	public Body getBody() {
-		return body;
+		return physics.getBody();
 	}
 	
 	public Contact getContact() {
-		return contact;
+		return physics.getContact();
+	}
+	
+	public Physics getPhysics() {
+		return physics;
 	}
 	
 	public PhysicsComponent(Body body) {
-		this.body = body;
-		this.contact = new Contact();
+		this(new PhysicsImpl(body));
+	}
+	
+	public PhysicsComponent(Physics physics) {
+		this.physics = physics;
 	}
 
 }
