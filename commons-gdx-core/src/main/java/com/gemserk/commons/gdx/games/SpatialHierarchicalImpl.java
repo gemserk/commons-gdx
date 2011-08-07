003d76999cab194ff159e301dfa6a32dab7e49e8
Ariel Coppes <ariel.coppes@gemserk.com>
2011-08-07 20:12:19 -0300

    extracted SpatialHierarchicalImpl class and moved it to package where Spatial is (package games?) moved test to core module tests folder from artemis module

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/games/SpatialHierarchicalImpl.java
@@ -0,0 +1,79 @@
+package com.gemserk.commons.gdx.games;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class SpatialHierarchicalImpl implements Spatial {
+
+	private static final Vector2 aux = new Vector2();
+
+	private final Spatial parent;
+
+	private final Vector2 localPosition = new Vector2();
+	private final Vector2 absolutePosition = new Vector2();
+
+	private float localAngle;
+	private float w, h;
+
+	public SpatialHierarchicalImpl(Spatial parent, float width, float height) {
+		this.parent = parent;
+		this.localAngle = 0f;
+		setPosition(parent.getX(), parent.getY());
+		setAngle(parent.getAngle());
+	}
+
+	@Override
+	public float getX() {
+		return getPosition().x;
+	}
+
+	@Override
+	public float getY() {
+		return getPosition().y;
+	}
+
+	@Override
+	public Vector2 getPosition() {
+		aux.set(localPosition);
+		aux.rotate(parent.getAngle());
+		absolutePosition.set(aux.x + parent.getX(), aux.y + parent.getY());
+		return absolutePosition;
+	}
+
+	@Override
+	public void setPosition(float x, float y) {
+		localPosition.set(x - parent.getX(), y - parent.getY());
+	}
+
+	@Override
+	public float getAngle() {
+		return localAngle + parent.getAngle();
+	}
+
+	@Override
+	public void setAngle(float angle) {
+		localAngle = angle - parent.getAngle();
+	}
+
+	@Override
+	public float getWidth() {
+		return w;
+	}
+
+	@Override
+	public float getHeight() {
+		return h;
+	}
+
+	@Override
+	public void setSize(float width, float height) {
+		this.w = width;
+		this.h = height;
+	}
+
+	@Override
+	public void set(Spatial spatial) {
+		setPosition(spatial.getX(), spatial.getY());
+		setAngle(spatial.getAngle());
+	}
+
+}
--- commons-gdx-core/src/test/java/com/gemserk/commons/gdx/games/HierarchicalSpatialTest.java
+++ commons-gdx-core/src/test/java/com/gemserk/commons/gdx/games/HierarchicalSpatialTest.java
@@ -1,4 +1,4 @@
-package com.gemserk.commons.artemis.systems;
+package com.gemserk.commons.gdx.games;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
@@ -6,87 +6,11 @@ import static org.junit.Assert.assertThat;
 import org.hamcrest.core.IsEqual;
 import org.junit.Test;
 
-import com.badlogic.gdx.math.Vector2;
 import com.gemserk.commons.gdx.games.Spatial;
+import com.gemserk.commons.gdx.games.SpatialHierarchicalImpl;
 import com.gemserk.commons.gdx.games.SpatialImpl;
 
 public class HierarchicalSpatialTest {
-
-	static class SpatialHierarchicalImpl implements Spatial {
-
-		private static final Vector2 aux = new Vector2();
-
-		private final Spatial parent;
-
-		private final Vector2 localPosition = new Vector2();
-		private final Vector2 absolutePosition = new Vector2();
-
-		private float localAngle;
-		private float w, h;
-
-		public SpatialHierarchicalImpl(Spatial parent, float width, float height) {
-			this.parent = parent;
-			this.localAngle = 0f;
-			setPosition(parent.getX(), parent.getY());
-			setAngle(parent.getAngle());
-		}
-
-		@Override
-		public float getX() {
-			return getPosition().x;
-		}
-
-		@Override
-		public float getY() {
-			return getPosition().y;
-		}
-
-		@Override
-		public Vector2 getPosition() {
-			aux.set(localPosition);
-			aux.rotate(parent.getAngle());
-			absolutePosition.set(aux.x + parent.getX(), aux.y + parent.getY());
-			return absolutePosition;
-		}
-
-		@Override
-		public void setPosition(float x, float y) {
-			localPosition.set(x - parent.getX(), y - parent.getY());
-		}
-
-		@Override
-		public float getAngle() {
-			return localAngle + parent.getAngle();
-		}
-
-		@Override
-		public void setAngle(float angle) {
-			localAngle = angle - parent.getAngle();
-		}
-
-		@Override
-		public float getWidth() {
-			return w;
-		}
-
-		@Override
-		public float getHeight() {
-			return h;
-		}
-
-		@Override
-		public void setSize(float width, float height) {
-			this.w = width;
-			this.h = height;
-		}
-
-		@Override
-		public void set(Spatial spatial) {
-			setPosition(spatial.getX(), spatial.getY());
-			setAngle(spatial.getAngle());
-		}
-
-	}
 
 	@Test
 	public void shouldReturnParentPosition() {
