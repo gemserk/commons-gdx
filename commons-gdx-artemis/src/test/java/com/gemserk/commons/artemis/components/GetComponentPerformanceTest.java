3835ca24ee7279bb758dd653e47ee5cd2c74211d
Ariel Coppes <ariel.coppes@gemserk.com>
2011-08-16 13:24:11 -0300

    added vm warm up before starting the tests, modified test iteratios to 50000 and added different logs, one for total time in ns, then ns average (iteration time) and finally total time in ms

--- commons-gdx-artemis/src/test/java/com/gemserk/commons/artemis/components/GetComponentPerformanceTest.java
+++ commons-gdx-artemis/src/test/java/com/gemserk/commons/artemis/components/GetComponentPerformanceTest.java
@@ -16,102 +16,110 @@ public class GetComponentPerformanceTest {
 	}
 
 	@Test
-	public void testTimeWithGetComponentFromClass() {
+	public void test() {
 
-		World world = new World();
-		Entity e = world.createEntity();
-		e.addComponent(new MyTestComponent());
-		e.refresh();
+		// some vm warm up
 
-		long startNanoTime = System.nanoTime();
-
-		for (int i = 0; i < 100000; i++) {
-			MyTestComponent myTestComponent = e.getComponent(MyTestComponent.class);
+		for (int i = 0; i < 100; i++) {
+			testTimeWithGetComponentFromClassWithClassCache(1000);
+			testTimeWithGetComponentFromClass(1000);
+			testTimeWithGetComponentFromMapper(1000);
+			testTimeWithGetComponentUsingTypeAndClass(1000);
 		}
 
-		long endNanoTime = System.nanoTime();
-		long totalTimeNs = (endNanoTime - startNanoTime);
-		long totalTimeMs = (endNanoTime - startNanoTime) / 1000000;
-		
-		System.out.println("Time with e.getComponent(class) = " + totalTimeNs + "ns");
-		System.out.println("Time with e.getComponent(class) = " + totalTimeMs + "ms");
+		int iterations = 50000;
 
+		long testTimeWithGetComponentFromMapperNs = testTimeWithGetComponentFromMapper(iterations);
+		long testTimeWithGetComponentUsingTypeAndClassNs = testTimeWithGetComponentUsingTypeAndClass(iterations);
+		long testTimeWithGetComponentFromClassWithClassCacheNs = testTimeWithGetComponentFromClassWithClassCache(iterations);
+		long testTimeWithGetComponentFromClassNs = testTimeWithGetComponentFromClass(iterations);
+
+		System.out.println("Time with e.getComponent(class) with class cache = " + testTimeWithGetComponentFromClassWithClassCacheNs + "ns");
+		System.out.println("Time with e.getComponent(class) = " + testTimeWithGetComponentFromClassNs + "ns");
+		System.out.println("Time with componentMapper.get(e) = " + testTimeWithGetComponentFromMapperNs + "ns");
+		System.out.println("Time with cast(e.getComponent(type)) = " + testTimeWithGetComponentUsingTypeAndClassNs + "ns");
+
+		System.out.println("Time with e.getComponent(class) with class cache = " + testTimeWithGetComponentFromClassWithClassCacheNs / iterations + "ns (average)");
+		System.out.println("Time with e.getComponent(class) = " + testTimeWithGetComponentFromClassNs / iterations + "ns (average)");
+		System.out.println("Time with componentMapper.get(e) = " + testTimeWithGetComponentFromMapperNs / iterations + "ns (average)");
+		System.out.println("Time with cast(e.getComponent(type)) = " + testTimeWithGetComponentUsingTypeAndClassNs / iterations + "ns (average)");
+
+		System.out.println("Time with e.getComponent(class) with class cache = " + testTimeWithGetComponentFromClassWithClassCacheNs / 1000000 + "ms");
+		System.out.println("Time with e.getComponent(class) = " + testTimeWithGetComponentFromClassNs / 1000000 + "ms");
+		System.out.println("Time with componentMapper.get(e) = " + testTimeWithGetComponentFromMapperNs / 1000000 + "ms");
+		System.out.println("Time with cast(e.getComponent(type)) = " + testTimeWithGetComponentUsingTypeAndClassNs / 1000000 + "ms");
 	}
-	
-	@Test
-	public void testTimeWithGetComponentFromClassWithClassCache() {
+
+	public long testTimeWithGetComponentFromMapper(int iterations) {
 
 		World world = new World();
 		Entity e = world.createEntity();
 		e.addComponent(new MyTestComponent());
 		e.refresh();
 
-		long startNanoTime = System.nanoTime();
-		
-		Class<MyTestComponent> myTestComponentClass = MyTestComponent.class;
-
-		for (int i = 0; i < 100000; i++) {
-			MyTestComponent myTestComponent = e.getComponent(myTestComponentClass);
-		}
-
-		long endNanoTime = System.nanoTime();
-		long totalTimeNs = (endNanoTime - startNanoTime);
-		long totalTimeMs = (endNanoTime - startNanoTime) / 1000000;
-		
-		System.out.println("Time with e.getComponent(class) with class cache = " + totalTimeNs + "ns");
-		System.out.println("Time with e.getComponent(class) with class cache = " + totalTimeMs + "ms");
-
-	}
-	
-	@Test
-	public void testTimeWithGetComponentFromMapper() {
-
-		World world = new World();
-		Entity e = world.createEntity();
-		e.addComponent(new MyTestComponent());
-		e.refresh();
-		
 		ComponentMapper<MyTestComponent> myTestComponentMapper = new ComponentMapper<MyTestComponent>(MyTestComponent.class, world.getEntityManager());
 
 		long startNanoTime = System.nanoTime();
 
-		for (int i = 0; i < 100000; i++) {
+		for (int i = 0; i < iterations; i++) {
 			MyTestComponent myTestComponent = myTestComponentMapper.get(e);
 		}
 
-		long endNanoTime = System.nanoTime();
-		long totalTimeNs = (endNanoTime - startNanoTime);
-		long totalTimeMs = (endNanoTime - startNanoTime) / 1000000;
-		
-		System.out.println("Time with componentMapper.get(e) = " + totalTimeNs + "ns");
-		System.out.println("Time with componentMapper.get(e) = " + totalTimeMs + "ms");
-
+		return System.nanoTime() - startNanoTime;
 	}
-	
-	@Test
-	public void testTimeWithGetComponentUsingTypeAndClass() {
+
+	public long testTimeWithGetComponentUsingTypeAndClass(int iterations) {
 
 		World world = new World();
 		Entity e = world.createEntity();
 		e.addComponent(new MyTestComponent());
 		e.refresh();
-		
+
 		ComponentType myTestComponentType = ComponentTypeManager.getTypeFor(MyTestComponent.class);
 		Class<MyTestComponent> myTestComponentClass = MyTestComponent.class;
 
 		long startNanoTime = System.nanoTime();
 
-		for (int i = 0; i < 100000; i++) {
+		for (int i = 0; i < iterations; i++) {
 			MyTestComponent myTestComponent = myTestComponentClass.cast(e.getComponent(myTestComponentType));
 		}
 
-		long endNanoTime = System.nanoTime();
-		long totalTimeNs = (endNanoTime - startNanoTime);
-		long totalTimeMs = (endNanoTime - startNanoTime) / 1000000;
-		
-		System.out.println("Time with cast(e.getComponent(type)) = " + totalTimeNs + "ns");
-		System.out.println("Time with cast(e.getComponent(type)) = " + totalTimeMs + "ms");
-
+		return System.nanoTime() - startNanoTime;
 	}
 
+	public long testTimeWithGetComponentFromClassWithClassCache(int iterations) {
+
+		World world = new World();
+		Entity e = world.createEntity();
+		e.addComponent(new MyTestComponent());
+		e.refresh();
+
+		Class<MyTestComponent> myTestComponentClass = MyTestComponent.class;
+
+		long startNanoTime = System.nanoTime();
+
+		for (int i = 0; i < iterations; i++) {
+			MyTestComponent myTestComponent = e.getComponent(myTestComponentClass);
+		}
+
+		return System.nanoTime() - startNanoTime;
+	}
+
+	public long testTimeWithGetComponentFromClass(int iterations) {
+
+		World world = new World();
+		Entity e = world.createEntity();
+		e.addComponent(new MyTestComponent());
+		e.refresh();
+
+		long startNanoTime = System.nanoTime();
+		long iterationsTime = 0;
+		long previousFrameTime = startNanoTime;
+
+		for (int i = 0; i < iterations; i++) {
+			MyTestComponent myTestComponent = e.getComponent(MyTestComponent.class);
+		}
+
+		return System.nanoTime() - startNanoTime;
+	}
 }
