1d0d8900757d64c9a8fe40e34f82d01ac9d62800
Ariel Coppes <ariel.coppes@gemserk.com>
2011-08-17 17:17:20 -0300

    added an Artemis system to register Script methods using reflection and stuff

---
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/ReflectionRegistratorEventSystem.java
@@ -0,0 +1,44 @@
+package com.gemserk.commons.artemis.systems;
+
+import com.artemis.Entity;
+import com.artemis.EntityProcessingSystem;
+import com.gemserk.commons.artemis.components.ScriptComponent;
+import com.gemserk.commons.artemis.events.EventListenerManager;
+import com.gemserk.commons.artemis.events.reflection.EventListenerReflectionRegistrator;
+import com.gemserk.commons.artemis.scripts.Script;
+
+public class ReflectionRegistratorEventSystem extends EntityProcessingSystem {
+
+	private static final Class<ScriptComponent> scriptComponentClass = ScriptComponent.class;
+	private final EventListenerManager eventListenerManager;
+	
+	public ReflectionRegistratorEventSystem(EventListenerManager eventListenerManager) {
+		super(ScriptComponent.class);
+		this.eventListenerManager = eventListenerManager;
+	}
+
+	@Override
+	protected void added(Entity e) {
+		super.added(e);
+		Script[] scripts = e.getComponent(scriptComponentClass).getScripts();
+		for (int i = 0; i < scripts.length; i++) {
+			Script script = scripts[i];
+			EventListenerReflectionRegistrator.registerEventListeners(script, eventListenerManager);
+		}
+	}
+
+	@Override
+	protected void removed(Entity e) {
+		Script[] scripts = e.getComponent(scriptComponentClass).getScripts();
+		for (int i = 0; i < scripts.length; i++) {
+			Script script = scripts[i];
+			EventListenerReflectionRegistrator.unregisterEventListeners(script, eventListenerManager);
+		}
+	}
+
+	@Override
+	protected void process(Entity e) {
+		
+	}
+
+}
