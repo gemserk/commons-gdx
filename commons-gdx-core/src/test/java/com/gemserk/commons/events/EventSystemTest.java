3794b3bcf3f55d55f1ef4737e35ab8ffaa4b5aa3
Ariel Coppes <ariel.coppes@gemserk.com>
2011-08-17 14:46:21 -0300

    modified to create the sender handler inside the event registry

--- commons-gdx-core/src/test/java/com/gemserk/commons/events/EventSystemTest.java
+++ commons-gdx-core/src/test/java/com/gemserk/commons/events/EventSystemTest.java
@@ -16,14 +16,6 @@ public class EventSystemTest {
 
 	class Event {
 
-		// Event();
-		// std::string mName;
-		// std::vector< MessageReceiver* > mReceivers;
-		// uint32 mSenderCount;
-		//
-		// // to be able to unregister yourself
-		// EventRegistry* mReg;
-
 		String name;
 		ArrayList<EventReceiver> receivers;
 		int senderCount;
@@ -39,11 +31,6 @@ public class EventSystemTest {
 
 	class SenderEventHandle {
 
-		// void SendMessage( Object* origin );
-
-		// friend EventRegistry;
-		// Event* mEvent;
-
 		Event event;
 
 		public SenderEventHandle() {
@@ -51,33 +38,15 @@ public class EventSystemTest {
 		}
 
 		void sendMessage(Object source) {
-
-			// if ( NULL != mEvent )
-			// {
-			// std::vector<MessageReceiver*>& rv = mEvent->mReceivers;
-			// int cnt = rv.size();
-			// for ( int i = 0 ; i != cnt ; ++i )
-			// {
-			// rv[i]->ReceiveMessage( mEvent, origin );
-			// }
-			// }
-
 			if (event == null)
 				return;
-
 			for (int i = 0; i < event.receivers.size(); i++)
 				event.receivers.get(i).receiveEvent(event, source);
-
 		}
 
 	}
 
 	class ReceiverEventHandle {
-
-		// friend EventRegistry;
-		// Event* mEvent;
-		// // to be able to auto-unregister
-		// MessageReceiver* mReceiver;
 
 		Event event;
 		EventReceiver eventReceiver;
@@ -91,8 +60,6 @@ public class EventSystemTest {
 
 	interface EventReceiver {
 
-		// virtual void ReceiveMessage( Event* e, Object* origin ) = 0;
-
 		void receiveEvent(Event e, Object source);
 
 	}
@@ -101,27 +68,19 @@ public class EventSystemTest {
 
 		Map<String, Event> events = new HashMap<String, Event>();
 
-		void register(String eventName, SenderEventHandle senderEventHandle) {
+		SenderEventHandle register(String eventName) {
 
-			// // 1. Unreg
-			// Unregister( eh );
-			//
-			// // 2. Re-reg
-			// Event& e = mEvents[ eventName ];
-			// e.mName = eventName; // if added, make sure name is set
-			// e.mReg = this;
-			// ++(e.mSenderCount);
-			//
-			// eh->mEvent = &e;
-
-			unregister(senderEventHandle);
+			// unregister(senderEventHandle);
 
 			Event event = getEvent(eventName);
 			event.name = eventName;
 			event.eventRegistry = this;
 			event.senderCount++;
 
+			SenderEventHandle senderEventHandle = new SenderEventHandle();
 			senderEventHandle.event = event;
+
+			return senderEventHandle;
 		}
 
 		private Event getEvent(String name) {
@@ -131,18 +90,6 @@ public class EventSystemTest {
 		}
 
 		void subscribe(String eventName, ReceiverEventHandle receiverEventHandle, EventReceiver eventReceiver) {
-
-			// // 1. Unsub
-			// Unsubscribe( eh, receiver );
-			//
-			// // 2. Re-reg
-			// Event& e = mEvents[ eventName ];
-			// e.mName = eventName; // if added, make sure name is set
-			// e.mReceivers.push_back( receiver );
-			// e.mReg = this;
-			//
-			// eh->mEvent = &e;
-			// eh->mReceiver = receiver;
 
 			unsubscribe(receiverEventHandle, eventReceiver);
 
@@ -158,19 +105,6 @@ public class EventSystemTest {
 
 		void unregister(SenderEventHandle senderEventHandle) {
 
-			// Event* e = eh->mEvent;
-			// if ( NULL!= e )
-			// {
-			// std::map< std::string, Event >::iterator it = mEvents.find( e->mName );
-			// --(e->mSenderCount);
-			// ASSERT( 0 <= e->mSenderCount ); // non-negative
-			// if ( 0 == e->mSenderCount && e->mReceivers.empty() )
-			// {
-			// mEvents.erase( it );
-			// }
-			// eh->mEvent = NULL;
-			// }
-
 			Event e = senderEventHandle.event;
 			if (e == null)
 				return;
@@ -184,25 +118,6 @@ public class EventSystemTest {
 		}
 
 		void unsubscribe(ReceiverEventHandle receiverEventHandle, EventReceiver eventReceiver) {
-
-			// Event* e = eh->mEvent;
-			// if ( NULL!= e )
-			// {
-			// std::map< std::string, Event >::iterator eit = mEvents.find( e->mName );
-			// ASSERT( eit != mEvents.end() ); // found
-			//
-			// std::vector<int>::iterator it;
-			// std::vector< MessageReceiver* >::iterator rit = std::find( e->mReceivers.begin(), e->mReceivers.end(), receiver );
-			// ASSERT( rit != e->mReceivers.end() ); // existed
-			//
-			// if ( 0 == e->mSenderCount && e->mReceivers.empty() )
-			// {
-			// mEvents.erase( eit );
-			// }
-			//
-			// eh->mEvent = NULL;
-			// eh->mReceiver = NULL;
-			// }
 
 			Event event = receiverEventHandle.event;
 			if (event == null)
@@ -225,21 +140,20 @@ public class EventSystemTest {
 	@Test
 	public void sendEventsTest() {
 
-		SenderEventHandle senderEventHandle = new SenderEventHandle();
 		ReceiverEventHandle receiverEventHandle = new ReceiverEventHandle();
 
 		EventRegistry eventRegistry = new EventRegistry();
 
-		eventRegistry.register("event1", senderEventHandle);
+		SenderEventHandle senderEventHandle = eventRegistry.register("event1");
 		eventRegistry.subscribe("event1", receiverEventHandle, new EventReceiver() {
-
 			@Override
 			public void receiveEvent(Event e, Object source) {
-				System.out.println("event1");
+				System.out.println("event1 " + source.toString());
 			}
 		});
 
-		senderEventHandle.sendMessage(null);
+		senderEventHandle.sendMessage("a");
+		senderEventHandle.sendMessage("b");
 
 	}
 
