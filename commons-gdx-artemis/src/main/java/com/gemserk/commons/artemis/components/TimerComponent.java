ba7de31452df8bfba0950c5d0d200cc9921aac1c
Ariel Coppes <ariel.coppes@gemserk.com>
2011-08-22 13:30:15 -0300

    removed unused TimerSystem, changed TimerComponent to use float and removed Trigger from it

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/TimerComponent.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/TimerComponent.java
@@ -1,50 +1,34 @@
 package com.gemserk.commons.artemis.components;
 
 import com.artemis.Component;
-import com.gemserk.commons.artemis.triggers.NullTrigger;
-import com.gemserk.commons.artemis.triggers.Trigger;
 
 public class TimerComponent extends Component {
-	
-	private final int time;
-	
-	private int currentTime;
-	
-	private Trigger trigger;
-	
-	public int getTime() {
-		return time;
-	}
-	
-	public int getCurrentTime() {
-		return currentTime;
-	}
-	
-	public void setCurrentTime(int currentTime) {
-		this.currentTime = currentTime;
-	}
-	
-	public boolean isFinished() {
-		return currentTime < 0;
-	}
-	
-	public void reset() {
-		currentTime = time;
-	}
-	
-	public Trigger getTrigger() {
-		return trigger;
-	}
-	
-	public TimerComponent(int time, Trigger trigger) {
-		this.time = time;
-		this.currentTime = time;
-		this.trigger = trigger;
+
+	private float totalTime;
+	private float currentTime;
+
+	public float getTotalTime() {
+		return totalTime;
 	}
 
-	public TimerComponent(int time) {
-		this.time = time;
+	public float getCurrentTime() {
+		return currentTime;
+	}
+
+	public void setCurrentTime(float currentTime) {
+		this.currentTime = currentTime;
+	}
+
+	public boolean isFinished() {
+		return currentTime <= 0;
+	}
+
+	public void reset() {
+		currentTime = totalTime;
+	}
+
+	public TimerComponent(float time) {
+		this.totalTime = time;
 		this.currentTime = time;
-		this.trigger = new NullTrigger();
 	}
 }
