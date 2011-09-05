f293778b2cb2ccda64558b30c939cfa2a2307e82
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-05 11:32:44 -0300

    added javadocs to SvgElementHandler

--- commons-gdx-core/src/main/java/com/gemserk/commons/svg/inkscape/SvgElementHandler.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/svg/inkscape/SvgElementHandler.java
@@ -2,9 +2,24 @@ package com.gemserk.commons.svg.inkscape;
 
 import org.w3c.dom.Element;
 
-
+/**
+ * Called by the SvgParser to handle a SvgElement.
+ * 
+ * @author acoppes
+ * 
+ */
 public interface SvgElementHandler {
 
+	/**
+	 * Called to handle the SvgElement.
+	 * 
+	 * @param svgParser
+	 *            The SvgParser to continue the SVG parsing or not.
+	 * @param svgElement
+	 *            The SvgElement to do stuff with.
+	 * @param element
+	 *            The XML element to get extra information.
+	 */
 	void handle(SvgParser svgParser, SvgElement svgElement, Element element);
 
 }
