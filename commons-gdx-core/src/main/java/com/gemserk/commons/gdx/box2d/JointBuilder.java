b0eb62c60a118229b689f85f7087037cfe17303a
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-22 15:44:39 -0300

    extracted common joint builder logic to another class, mainly to keep the same logic in the same place but it seems like it was not so necesary

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/box2d/JointBuilder.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/box2d/JointBuilder.java
@@ -2,6 +2,7 @@ package com.gemserk.commons.gdx.box2d;
 
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Joint;
+import com.badlogic.gdx.physics.box2d.JointDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.joints.DistanceJointDef;
 import com.badlogic.gdx.physics.box2d.joints.RopeJointDef;
@@ -12,31 +13,57 @@ import com.badlogic.gdx.physics.box2d.joints.RopeJointDef;
  * @author acoppes
  */
 public class JointBuilder {
-	
+
+	private class InternalJointBuilder {
+
+		JointDef jointDef;
+
+		public void setJointDef(JointDef jointDef) {
+			this.jointDef = jointDef;
+		}
+
+		public InternalJointBuilder bodyA(Body bodyA) {
+			jointDef.bodyA = bodyA;
+			return this;
+		}
+
+		public InternalJointBuilder bodyB(Body bodyB) {
+			jointDef.bodyB = bodyB;
+			return this;
+		}
+
+		public InternalJointBuilder collideConnected(boolean collideConnected) {
+			jointDef.collideConnected = collideConnected;
+			return this;
+		}
+	}
+
 	public class DistanceJointBuilder {
-		
+
+		private InternalJointBuilder internalJointBuilder = new InternalJointBuilder();
 		private DistanceJointDef distanceJointDef;
 
 		private void reset() {
 			// TODO: reuse the same distance Joint by setting default values here..
 			distanceJointDef = new DistanceJointDef();
+			internalJointBuilder.setJointDef(distanceJointDef);
 		}
-		
+
 		public DistanceJointBuilder bodyA(Body bodyA) {
-			distanceJointDef.bodyA = bodyA;
+			internalJointBuilder.bodyA(bodyA);
 			return this;
 		}
 
 		public DistanceJointBuilder bodyB(Body bodyB) {
-			distanceJointDef.bodyB = bodyB;
+			internalJointBuilder.bodyB(bodyB);
 			return this;
 		}
-		
+
 		public DistanceJointBuilder collideConnected(boolean collideConnected) {
-			distanceJointDef.collideConnected = collideConnected;
+			internalJointBuilder.collideConnected(collideConnected);
 			return this;
 		}
-		
+
 		public DistanceJointBuilder length(float length) {
 			distanceJointDef.length = length;
 			return this;
@@ -46,7 +73,7 @@ public class JointBuilder {
 			distanceJointDef.frequencyHz = frequencyHz;
 			return this;
 		}
-		
+
 		public DistanceJointBuilder dampingRatio(float dampingRatio) {
 			distanceJointDef.dampingRatio = dampingRatio;
 			return this;
@@ -55,71 +82,73 @@ public class JointBuilder {
 		public Joint build() {
 			return world.createJoint(distanceJointDef);
 		}
-		
+
 	}
-	
+
 	public class RopeJointBuilder {
-		
+
+		private InternalJointBuilder internalJointBuilder = new InternalJointBuilder();
 		private RopeJointDef ropeJointDef;
 
 		private void reset() {
 			// TODO: reuse the same distance Joint by setting default values here..
 			ropeJointDef = new RopeJointDef();
+			internalJointBuilder.setJointDef(ropeJointDef);
 		}
-		
+
 		public RopeJointBuilder bodyA(Body bodyA) {
-			ropeJointDef.bodyA = bodyA;
+			internalJointBuilder.bodyA(bodyA);
 			return this;
 		}
 
 		public RopeJointBuilder bodyB(Body bodyB) {
-			ropeJointDef.bodyB = bodyB;
+			internalJointBuilder.bodyB(bodyB);
 			return this;
 		}
-		
+
 		public RopeJointBuilder bodyA(Body bodyA, float lx, float ly) {
-			ropeJointDef.bodyA = bodyA;
+			internalJointBuilder.bodyA(bodyA);
 			ropeJointDef.localAnchorA.set(lx, ly);
 			return this;
 		}
-		
+
 		public RopeJointBuilder bodyB(Body bodyB, float lx, float ly) {
-			ropeJointDef.bodyB = bodyB;
+			internalJointBuilder.bodyB(bodyB);
 			ropeJointDef.localAnchorB.set(lx, ly);
 			return this;
 		}
-		
+
 		public RopeJointBuilder collideConnected(boolean collideConnected) {
-			ropeJointDef.collideConnected = collideConnected;
+			internalJointBuilder.collideConnected(collideConnected);
 			return this;
 		}
-		
+
 		public RopeJointBuilder maxLength(float maxLength) {
 			ropeJointDef.maxLength = maxLength;
 			return this;
 		}
-		
+
 		public Joint build() {
 			return world.createJoint(ropeJointDef);
 		}
-		
+
 	}
 
 	private final World world;
 	private DistanceJointBuilder distanceJointBuilder;
 	private RopeJointBuilder ropeJointBuilder;
-	
+
 	public JointBuilder(World world) {
 		this.world = world;
 		this.distanceJointBuilder = new DistanceJointBuilder();
 		this.ropeJointBuilder = new RopeJointBuilder();
 	}
-	
+
 	public DistanceJointBuilder distanceJoint() {
 		distanceJointBuilder.reset();
 		return distanceJointBuilder;
 	}
-	
+
 	public RopeJointBuilder ropeJointBuilder() {
 		ropeJointBuilder.reset();
 		return ropeJointBuilder;
