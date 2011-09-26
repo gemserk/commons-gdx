d3564f6b34d8f1ab71dd1c59e709e4c548b51a42
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-26 18:06:58 -0300

    changed the SpriteUpdateSystem to try to interpolate sprite spatial if there is information to interpolate

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/SpriteUpdateSystem.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/SpriteUpdateSystem.java
@@ -1,46 +1,66 @@
 package com.gemserk.commons.artemis.systems;
 
-import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.EntityProcessingSystem;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
+import com.gemserk.animation4j.interpolator.FloatInterpolator;
+import com.gemserk.commons.artemis.components.Components;
+import com.gemserk.commons.artemis.components.PreviousStateSpatialComponent;
 import com.gemserk.commons.artemis.components.SpatialComponent;
 import com.gemserk.commons.artemis.components.SpriteComponent;
 import com.gemserk.commons.gdx.games.Spatial;
+import com.gemserk.commons.gdx.time.TimeStepProvider;
+import com.gemserk.commons.gdx.time.TimeStepProviderGlobalImpl;
 
+/**
+ * Updates Sprites from SpriteComponent to the location of the Spatial from the SpatialComponent, if the entity has a PreviousSpatialStateComponent, then it performs an interpolation between both spatial information using the GlobalTime.getAlpha().
+ */
 public class SpriteUpdateSystem extends EntityProcessingSystem {
 
-	ComponentMapper<SpatialComponent> spatialComponentMapper;
-	ComponentMapper<SpriteComponent> spriteComponentMapper;
+	private final TimeStepProvider timeStepProvider;
 
-	@SuppressWarnings("unchecked")
 	public SpriteUpdateSystem() {
-		super(SpatialComponent.class, SpriteComponent.class);
+		this(new TimeStepProviderGlobalImpl());
 	}
 
-	@Override
-	public void initialize() {
-		spatialComponentMapper = new ComponentMapper<SpatialComponent>(SpatialComponent.class, world.getEntityManager());
-		spriteComponentMapper = new ComponentMapper<SpriteComponent>(SpriteComponent.class, world.getEntityManager());
+	@SuppressWarnings("unchecked")
+	public SpriteUpdateSystem(TimeStepProvider timeStepProvider) {
+		super(Components.spatialComponentClass, Components.spriteComponentClass);
+		this.timeStepProvider = timeStepProvider;
 	}
 
 	@Override
 	protected void process(Entity e) {
-		SpatialComponent spatialComponent = spatialComponentMapper.get(e);
-		SpriteComponent spriteComponent = spriteComponentMapper.get(e);
+		SpatialComponent spatialComponent = Components.spatialComponent(e);
+		SpriteComponent spriteComponent = Components.spriteComponent(e);
+
+		PreviousStateSpatialComponent previousStateSpatialComponent = Components.getPreviousStateSpatialComponent(e);
 
 		Spatial spatial = spatialComponent.getSpatial();
+
+		float newX = spatial.getX();
+		float newY = spatial.getY();
+
+		float angle = spatial.getAngle();
+
+		if (previousStateSpatialComponent != null) {
+			float interpolationAlpha = timeStepProvider.getAlpha();
+			Spatial previousSpatial = previousStateSpatialComponent.getSpatial();
+			newX = FloatInterpolator.interpolate(previousSpatial.getX(), spatial.getX(), interpolationAlpha);
+			newY = FloatInterpolator.interpolate(previousSpatial.getY(), spatial.getY(), interpolationAlpha);
+			angle = FloatInterpolator.interpolate(previousSpatial.getAngle(), spatial.getAngle(), interpolationAlpha);
+		}
 
 		Sprite sprite = spriteComponent.getSprite();
 		Vector2 center = spriteComponent.getCenter();
 
 		if (spriteComponent.isUpdateRotation())
-			sprite.setRotation(spatial.getAngle());
+			sprite.setRotation(angle);
+
 		sprite.setOrigin(spatial.getWidth() * center.x, spatial.getHeight() * center.y);
 
 		sprite.setSize(spatial.getWidth(), spatial.getHeight());
-		sprite.setPosition(spatial.getX() - sprite.getOriginX(), spatial.getY() - sprite.getOriginY());
-
+		sprite.setPosition(newX - sprite.getOriginX(), newY - sprite.getOriginY());
 	}
 }
