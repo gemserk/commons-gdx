bcb775b3311634efdc8c6b6e3c30e438811ae345
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-26 12:00:54 -0300

    added some methods to set width/height of a panel

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/gui/Panel.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/gui/Panel.java
@@ -3,9 +3,10 @@ package com.gemserk.commons.gdx.gui;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Panel extends Container {
-	
+
 	private float x, y;
-	
+	private float width, height;
+
 	public Panel(float x, float y) {
 		this.x = x;
 		this.y = y;
@@ -15,11 +16,27 @@ public class Panel extends Container {
 	public float getX() {
 		return x;
 	}
-	
+
 	public float getY() {
 		return y;
 	}
 	
+	public void setWidth(float width) {
+		this.width = width;
+	}
+	
+	public float getWidth() {
+		return width;
+	}
+	
+	public void setHeight(float height) {
+		this.height = height;
+	}
+	
+	public float getHeight() {
+		return height;
+	}
+
 	@Override
 	public void draw(SpriteBatch spriteBatch) {
 		super.draw(spriteBatch);
@@ -32,5 +49,13 @@ public class Panel extends Container {
 		control.setPosition(x + cx, y + cy);
 		super.add(control);
 	}
-	
+
+	@Override
+	public void setPosition(float x, float y) {
+		for (int i = 0; i < getControls().size(); i++) {
+			Control control = getControls().get(i);
+			control.setPosition(x + control.getX(), y + control.getY());
+		}
+	}
+
 }
