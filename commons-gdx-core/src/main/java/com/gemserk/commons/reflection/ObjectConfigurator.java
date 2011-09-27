4a2ad19b69ae9f169a3853660497221ccee486d9
Ariel Coppes <ariel.coppes@gemserk.com>
2011-09-27 14:51:25 -0300

    extracted ObjectConfigurator from the test class to its own file in commons.reflection package

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/reflection/ObjectConfigurator.java
@@ -0,0 +1,64 @@
+package com.gemserk.commons.reflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.gemserk.animation4j.reflection.ReflectionUtils;
+import com.gemserk.componentsengine.utils.RandomAccessMap;
+
+public class ObjectConfigurator {
+
+	protected static final Logger logger = LoggerFactory.getLogger(ObjectConfigurator.class);
+
+	private RandomAccessMap<String, Object> configurationMap;
+
+	public ObjectConfigurator() {
+		configurationMap = new RandomAccessMap<String, Object>();
+	}
+
+	public void configure(Object object) {
+
+		Class<?> clazz = object.getClass();
+		Field[] fields = clazz.getDeclaredFields();
+
+		for (int i = 0; i < fields.length; i++) {
+			Field field = fields[i];
+			String fieldName = field.getName();
+			Object value = configurationMap.get(fieldName);
+
+			if (value == null) {
+				logger.debug("Couldn't find value for field " + fieldName + " from " + clazz);
+				continue;
+			}
+
+			setField(object, fieldName, value);
+		}
+
+	}
+
+	public void setField(Object object, String fieldName, Object value) {
+		Class<?> clazz = object.getClass();
+		Method setter = ReflectionUtils.getSetter(clazz, fieldName);
+
+		String setterName = ReflectionUtils.getSetterName(fieldName);
+
+		if (setter == null) {
+			logger.debug("Failed to set value, " + setterName + "() method not found in " + object.getClass());
+			return;
+		}
+
+		try {
+			setter.invoke(object, value);
+		} catch (Exception e) {
+			throw new RuntimeException("failed to set value on " + setterName + "() method from " + object.getClass(), e);
+		}
+	}
+
+	public void add(String name, Object o) {
+		configurationMap.put(name, o);
+	}
+
+}
