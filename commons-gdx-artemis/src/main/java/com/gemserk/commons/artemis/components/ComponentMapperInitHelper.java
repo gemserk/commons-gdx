c5726fcb0bd381507eac4adaae4f25f412b07927
Ruben Garat <ruben.garat@gemserk.com>
2011-09-29 20:16:19 -0300

    added ComponentMapperInitHelper from artemis-extras

---
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/ComponentMapperInitHelper.java
@@ -0,0 +1,32 @@
+package com.gemserk.commons.artemis.components;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+
+import com.artemis.Component;
+import com.artemis.ComponentMapper;
+import com.artemis.EntityManager;
+
+public class ComponentMapperInitHelper {
+
+	public static void config(Object target, EntityManager entityManager) {
+		try {
+			for (Field field : target.getClass().getDeclaredFields()) {
+				String fieldName = field.getName();
+				Class<?> fieldType = field.getType();
+				if (ComponentMapper.class.isAssignableFrom(fieldType)) {
+					System.out.println("ComponentMapper detected: " + fieldName);
+					ParameterizedType genericType = (ParameterizedType) field.getGenericType();
+					Class componentType = (Class) genericType.getActualTypeArguments()[0];
+
+					ComponentMapper<Component> componentMapper = new ComponentMapper<Component>(componentType, entityManager);
+					field.setAccessible(true);
+					field.set(target, componentMapper);
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Error while setting component mappers", e);
+		}
+	}
+
+}
