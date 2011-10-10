f22bf7671cf59e2542647be8f33da2129519a865
Ariel Coppes <ariel.coppes@gemserk.com>
2011-10-10 11:27:19 -0200

    fixed Panel to do no logic at all and use the Container logic (should be removed)

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/gui/Panel.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/gui/Panel.java
@@ -4,41 +4,31 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Panel extends Container {
 
-	private float x, y;
 	private float width, height;
 
 	public Panel(float x, float y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public Panel(String id, float x, float y) {
-		super(id);
-		this.x = x;
-		this.y = y;
-	}
-	
-	@Override
-	public float getX() {
-		return x;
+		setX(x);
+		setY(y);
 	}
 
-	public float getY() {
-		return y;
+	public Panel(String id, float x, float y) {
+		super(id);
+		setX(x);
+		setY(y);
 	}
-	
+
 	public void setWidth(float width) {
 		this.width = width;
 	}
-	
+
 	public float getWidth() {
 		return width;
 	}
-	
+
 	public void setHeight(float height) {
 		this.height = height;
 	}
-	
+
 	public float getHeight() {
 		return height;
 	}
@@ -46,41 +36,6 @@ public class Panel extends Container {
 	@Override
 	public void draw(SpriteBatch spriteBatch) {
 		super.draw(spriteBatch);
-	}
-
-	@Override
-	public void add(Control control) {
-		float cx = control.getX();
-		float cy = control.getY();
-		control.setPosition(x + cx, y + cy);
-		super.add(control);
-	}
-
-	@Override
-	public void setPosition(float x, float y) {
-		for (int i = 0; i < getControls().size(); i++) {
-			Control control = getControls().get(i);
-			control.setPosition(x + control.getX(), y + control.getY());
-		}
-	}
-	
-	@Override
-	public void setX(float x) {
-		setPosition(x, this.y);
-	}
-
-	@Override
-	public void setY(float y) {
-		setPosition(this.x, y);
-	}
-	
-	@Override
-	public void invalidate() {
-		super.invalidate();
-		for (int i = 0; i < getControls().size(); i++) {
-			Control control = getControls().get(i);
-			control.setPosition(x + control.getX(), y + control.getY());
-		}
 	}
 
 }
