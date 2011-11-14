980c365eef59a84a02b39585662307eb62150ec6
Ariel Coppes <ariel.coppes@gemserk.com>
2011-11-14 10:02:59 -0200

    added interface of SceneTemplate and base implementation SceneTemplateImpl

---
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/templates/scenes/SceneTemplate.java
@@ -0,0 +1,26 @@
+package com.gemserk.commons.artemis.templates.scenes;
+
+import com.gemserk.commons.artemis.WorldWrapper;
+import com.gemserk.componentsengine.utils.Parameters;
+
+/**
+ * Represents the concept of a fixed template for a scene of a game.
+ * 
+ * @author acoppes
+ * 
+ */
+public interface SceneTemplate {
+
+	/**
+	 * Applies the scene template to the Artemis World stored in the WorldWrapper.
+	 * 
+	 * @param worldWrapper
+	 */
+	void apply(WorldWrapper worldWrapper);
+
+	/**
+	 * Returns an instance of Parameters to configure the needed parameters to apply the template.
+	 */
+	Parameters getParameters();
+
+}
---
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/templates/scenes/SceneTemplateImpl.java
@@ -0,0 +1,30 @@
+package com.gemserk.commons.artemis.templates.scenes;
+
+import com.gemserk.commons.artemis.WorldWrapper;
+import com.gemserk.componentsengine.utils.Parameters;
+import com.gemserk.componentsengine.utils.ParametersWrapper;
+
+/**
+ * Base implementation of the SceneTemplate class.
+ * @author acoppes
+ *
+ */
+public class SceneTemplateImpl implements SceneTemplate {
+	
+	Parameters parameters;
+	
+	public SceneTemplateImpl() {
+		parameters = new ParametersWrapper();
+	}
+
+	@Override
+	public void apply(WorldWrapper worldWrapper) {
+		
+	}
+
+	@Override
+	public Parameters getParameters() {
+		return parameters;
+	}
+
+}
