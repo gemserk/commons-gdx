7e4526c0d4f46b7e1beca3d2b4a958425a55ce43
Ariel Coppes <ariel.coppes@gemserk.com>
2011-11-21 18:12:37 -0200

    added default null implementation of FacebookUtils interface

--- commons-gdx-core/src/main/java/com/gemserk/commons/utils/FacebookUtils.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/utils/FacebookUtils.java
@@ -1,7 +1,16 @@
 package com.gemserk.commons.utils;
 
+/**
+ * Common utilities to work with Facebook.
+ */
 public interface FacebookUtils {
-	
+
+	/**
+	 * Opens the corresponding Facebook page given the oage identifier.
+	 * 
+	 * @param page
+	 *            The Facebook page identifier.
+	 */
 	void openPage(String page);
 
 }
---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/utils/FacebookUtilsNullImpl.java
@@ -0,0 +1,13 @@
+package com.gemserk.commons.utils;
+
+/**
+ * Null implementation of FacebookUtils class.
+ */
+public class FacebookUtilsNullImpl implements FacebookUtils {
+
+	@Override
+	public void openPage(String page) {
+
+	}
+
+}
