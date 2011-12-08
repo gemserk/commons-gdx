00f786f0b82037606bf18225194a9ccf1b162eba
Ariel Coppes <ariel.coppes@gemserk.com>
2011-12-08 20:10:36 -0200

    added an implementation of ApplicationListener based in different game states using the GameState (without using the Screen interface and implementation at all)

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/ApplicationListenerGameStateBasedImpl.java
@@ -0,0 +1,84 @@
+package com.gemserk.commons.gdx;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+
+/**
+ * Implementation of ApplicationListener based different game states using the GameState class.
+ */
+public class ApplicationListenerGameStateBasedImpl implements ApplicationListener {
+
+	protected GameState gameState;
+
+	public GameState getGameState() {
+		return gameState;
+	}
+
+	@Override
+	public void dispose() {
+		if (gameState == null)
+			return;
+		gameState.hide();
+		gameState.pause();
+		gameState.dispose();
+	}
+
+	@Override
+	public void pause() {
+		if (gameState != null)
+			gameState.pause();
+	}
+
+	@Override
+	public void resume() {
+		if (gameState != null)
+			gameState.resume();
+	}
+
+	@Override
+	public void render() {
+		if (gameState == null)
+			return;
+		gameState.setDelta(Gdx.graphics.getDeltaTime());
+		gameState.update();
+		gameState.render();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		if (gameState != null)
+			gameState.resize(width, height);
+	}
+
+	public void setGameState(GameState gameState) {
+		setGameState(gameState, false);
+	}
+
+	public void setGameState(GameState gameState, boolean disposeCurrent) {
+		if (gameState == null)
+			throw new IllegalArgumentException("Can't set null GameState");
+
+		GameState currentGameState = getGameState();
+
+		if (currentGameState == gameState)
+			return;
+
+		if (currentGameState != null) {
+			currentGameState.pause();
+			currentGameState.hide();
+			if (disposeCurrent)
+				currentGameState.dispose();
+		}
+
+		this.gameState = gameState;
+		gameState.init();
+		gameState.resume();
+		gameState.show();
+	}
+
+	@Override
+	public void create() {
+
+	}
+
+}
