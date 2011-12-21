bed0e504f181e7500da26362a9fad2787baab07c
Ariel Coppes <ariel.coppes@gemserk.com>
2011-12-21 15:56:38 -0200

    added textureWrap setting when declaring a texture using a texture resource builder

--- commons-gdx-resources/src/main/java/com/gemserk/commons/gdx/resources/TextureResourceBuilder.java
+++ commons-gdx-resources/src/main/java/com/gemserk/commons/gdx/resources/TextureResourceBuilder.java
@@ -3,14 +3,18 @@ package com.gemserk.commons.gdx.resources;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.Texture.TextureWrap;
 
 public class TextureResourceBuilder implements ResourceBuilder<Texture> {
-	
+
 	FileHandle fileHandle;
-	
+
 	TextureFilter minFilter = TextureFilter.Nearest;
 	TextureFilter magFilter = TextureFilter.Nearest;
-	
+
+	TextureWrap uTextureWrap = TextureWrap.ClampToEdge;
+	TextureWrap vTextureWrap = TextureWrap.ClampToEdge;
+
 	public TextureResourceBuilder minFilter(TextureFilter minFilter) {
 		this.minFilter = minFilter;
 		return this;
@@ -21,19 +25,26 @@ public class TextureResourceBuilder implements ResourceBuilder<Texture> {
 		return this;
 	}
 
+	public TextureResourceBuilder textureWrap(TextureWrap u, TextureWrap v) {
+		uTextureWrap = u;
+		vTextureWrap = v;
+		return this;
+	}
+
 	@Override
 	public boolean isVolatile() {
 		return false;
 	}
-	
+
 	public TextureResourceBuilder(FileHandle fileHandle) {
 		this.fileHandle = fileHandle;
 	}
-	
+
 	@Override
 	public Texture build() {
 		Texture texture = new Texture(fileHandle);
 		texture.setFilter(minFilter, magFilter);
+		texture.setWrap(uTextureWrap, vTextureWrap);
 		return texture;
 	}
 
