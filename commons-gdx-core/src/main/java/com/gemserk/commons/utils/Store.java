c84faf9afef3a6b0271c9b25817a06952fde23f1
Ariel Coppes <ariel.coppes@gemserk.com>
2011-12-24 23:22:05 -0200

    extracted protected methods reuseObject() and newObject() in the Store to be able to implement custom stuff in subclasses

--- commons-gdx-core/src/main/java/com/gemserk/commons/utils/Store.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/utils/Store.java
@@ -6,18 +6,25 @@ public abstract class Store<T> {
 
 	protected ArrayList<T> created = new ArrayList<T>();
 	protected ArrayList<T> free = new ArrayList<T>();
-	
+
 	public T get() {
-		if (free.isEmpty()) {
-			T t = createObject();
-			created.add(t);
-			return t;
-		} else {
-			T t = free.get(0);
-			free.remove(0);
-			created.add(t);
-			return t;
-		}
+		if (free.isEmpty())
+			return newObject();
+		else
+			return reuseObject();
+	}
+
+	protected T reuseObject() {
+		T t = free.get(0);
+		free.remove(0);
+		created.add(t);
+		return t;
+	}
+
+	protected T newObject() {
+		T t = createObject();
+		created.add(t);
+		return t;
 	}
 
 	protected abstract T createObject();
