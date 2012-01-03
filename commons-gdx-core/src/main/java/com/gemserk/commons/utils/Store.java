9a3fe538ceb8c6a2ca9b7e56db9be12f1466c2cf
Ariel Coppes <ariel.coppes@gemserk.com>
2012-01-03 10:18:29 -0200

    added javadocs to Store and StoreFactory and changed some fields to be package protected instead protected

--- commons-gdx-core/src/main/java/com/gemserk/commons/utils/Store.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/utils/Store.java
@@ -2,17 +2,34 @@ package com.gemserk.commons.utils;
 
 import java.util.ArrayList;
 
+/**
+ * Provides an API for a store of free objects and created objects of one kind, used to easily reuse them to avoid generating garbage collection.
+ */
+/**
+ * @author acoppes
+ * 
+ * @param <T>
+ */
 public class Store<T> {
-	
-	protected ArrayList<T> created = new ArrayList<T>();
-	protected ArrayList<T> free = new ArrayList<T>();
-	
+
+	ArrayList<T> created = new ArrayList<T>();
+	ArrayList<T> free = new ArrayList<T>();
+
 	StoreFactory<T> storeFactory;
-	
+
+	/**
+	 * Creates a new Store specifying the StoreFactory to be used when creating new objects.
+	 * 
+	 * @param storeFactory
+	 *            The StoreFactory to be used when creating new objects.
+	 */
 	public Store(StoreFactory<T> storeFactory) {
 		this.storeFactory = storeFactory;
 	}
 
+	/**
+	 * Returns an object from the Store, it reuses an object from the free collection if there is one or creates a new object otherwise.
+	 */
 	public T get() {
 		if (free.isEmpty())
 			return newObject();
@@ -33,24 +50,46 @@ public class Store<T> {
 		return t;
 	}
 
+	/**
+	 * Frees the object by returning it to the free objects collection.
+	 * 
+	 * @param t
+	 */
 	public void free(T t) {
-		// entities keep being in the world with this.
 		free.add(t);
 		created.remove(t);
 	}
 
+	/**
+	 * Returns the size of the created objects collection.
+	 */
 	public int size() {
 		return created.size();
 	}
-	
+
+	/**
+	 * Returns the total size of objects between free and created objects.
+	 */
 	public int getTotalSize() {
 		return created.size() + free.size();
 	}
 
+	/**
+	 * Returns an object of the created collection.
+	 * 
+	 * @param index
+	 *            The index of the object to be returned.
+	 */
 	public T get(int index) {
 		return created.get(index);
 	}
-	
+
+	/**
+	 * Creates the specified number of objects and adds them to the free collection in the store.
+	 * 
+	 * @param count
+	 *            The number of objects to be created.
+	 */
 	public void preCreate(int count) {
 		for (int i = 0; i < count; i++)
 			free(storeFactory.createObject());
--- commons-gdx-core/src/main/java/com/gemserk/commons/utils/StoreFactory.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/utils/StoreFactory.java
@@ -1,7 +1,13 @@
 package com.gemserk.commons.utils;
 
+/**
+ * Holds the logic of how to create an object, used by the Store<T>
+ */
 public interface StoreFactory<T> {
-	
+
+	/**
+	 * Returns a new object of the corresponding type.
+	 */
 	T createObject();
-	
+
 }
