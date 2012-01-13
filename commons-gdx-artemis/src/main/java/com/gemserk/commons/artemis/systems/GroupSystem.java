4d0c8ae2b1da9167c9f7829f7e94a357625b0d19
Ariel Coppes <ariel.coppes@gemserk.com>
2012-01-13 14:11:33 -0200

    added a GroupSystem similar to TagSystem

---
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/GroupSystem.java
@@ -0,0 +1,38 @@
+package com.gemserk.commons.artemis.systems;
+
+import com.artemis.Entity;
+import com.artemis.EntityProcessingSystem;
+import com.gemserk.commons.artemis.components.Components;
+import com.gemserk.commons.artemis.components.GroupComponent;
+
+public class GroupSystem extends EntityProcessingSystem {
+
+	@SuppressWarnings("unchecked")
+	public GroupSystem() {
+		super(Components.groupComponentClass);
+	}
+
+	@Override
+	protected void initialize() {
+		super.initialize();
+	}
+	
+	@Override
+	protected void enabled(Entity e) {
+		super.enabled(e);
+		GroupComponent groupComponent = Components.getGroupComponent(e);
+		world.getGroupManager().set(groupComponent.group, e);
+	}
+	
+	@Override
+	protected void disabled(Entity e) {
+		super.disabled(e);
+		world.getGroupManager().remove(e);
+	}
+
+	@Override
+	protected void process(Entity e) {
+
+	}
+
+}
