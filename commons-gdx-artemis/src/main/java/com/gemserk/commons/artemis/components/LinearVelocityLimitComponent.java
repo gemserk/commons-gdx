151a4d21bd068424ea4d5bbbd4a322e2583ba900
Ariel Coppes <ariel.coppes@gemserk.com>
2012-01-15 15:17:55 -0200

    modified limitlinearvelocity component to be able to set the limit

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/LinearVelocityLimitComponent.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/LinearVelocityLimitComponent.java
@@ -4,10 +4,14 @@ import com.artemis.Component;
 
 public class LinearVelocityLimitComponent extends Component {
 	
-	private final float limit;
+	private float limit;
 	
 	public float getLimit() {
 		return limit;
+	}
+	
+	public void setLimit(float limit) {
+		this.limit = limit;
 	}
 
 	public LinearVelocityLimitComponent(float limit) {
