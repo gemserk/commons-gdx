c82f64a1cf1351934b9ac12ab17f4a35ae338704
Ariel Coppes <ariel.coppes@gemserk.com>
2012-01-18 12:02:13 -0200

    added a class with useful methods to randomize an element of a set and an element of an array

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/utils/RandomUtils.java
@@ -0,0 +1,43 @@
+package com.gemserk.commons.utils;
+
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * Provides some useful functions when using random stuff.
+ */
+public class RandomUtils {
+
+	private static final Random random = new Random();
+
+	/**
+	 * Returns a random element of a Set.
+	 */
+	public static <T> T random(Set<T> set) {
+		int size = set.size();
+
+		if (size <= 0)
+			throw new IllegalStateException("Can't get a random item of an empty Set");
+
+		int randomIndex = random.nextInt(size);
+
+		int i = 0;
+		for (T t : set) {
+			if (i == randomIndex)
+				return t;
+			i++;
+		}
+
+		throw new IllegalStateException("This case should never happen if the Set has elements");
+	}
+
+	/**
+	 * Provides a random element of an array.
+	 */
+	public static <T> T random(T[] array) {
+		if (array.length == 0)
+			throw new IllegalStateException("Can't get a random item of an empty array");
+		return array[random.nextInt(array.length)];
+	}
+	
+}
