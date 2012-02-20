ac84c48392e43c5529a7d503932c45e40a6901fe
Ariel Coppes <ariel.coppes@gemserk.com>
2012-02-20 17:02:16 -0200

    added another common scene2d actor to Actors factory class, in this case a two options dialog

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/scene2d/Actors.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/scene2d/Actors.java
@@ -3,8 +3,11 @@ package com.gemserk.commons.gdx.scene2d;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Align;
+import com.badlogic.gdx.scenes.scene2d.ui.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.ui.FlickScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.gemserk.animation4j.gdx.scenes.scene2d.Scene2dConverters;
 import com.gemserk.animation4j.interpolator.function.InterpolationFunctions;
@@ -69,5 +72,57 @@ public class Actors {
 		};
 
 	}
+	
+	public static interface DialogListener {
+		
+		void optionSelected(int option);
+		
+	}
+	
+	public static Actor twoOptionsDialog(String[] texts, final DialogListener dialogListener, String titleText, String firstOption, String secondOption, Skin skin) {
+		Window window = new Window(titleText, skin);
+
+		window.setMovable(false);
+
+		TextButton firstOptionButton = new TextButton(firstOption, skin);
+		TextButton secondOptionButton = new TextButton(secondOption, skin);
+
+		firstOptionButton.setClickListener(new ClickListener() {
+			@Override
+			public void click(Actor actor, float x, float y) {
+				dialogListener.optionSelected(0);
+			}
+		});
+
+		secondOptionButton.setClickListener(new ClickListener() {
+			@Override
+			public void click(Actor actor, float x, float y) {
+				dialogListener.optionSelected(1);
+			}
+		});
+
+		window.defaults().spaceBottom(10);
+		window.row().fill().expandX();
+
+		for (int i = 0; i < texts.length; i++) {
+			window.row().padLeft(20);
+			Label label = new Label(texts[i], skin);
+			window.add(label).align(Align.LEFT).colspan(2);
+		}
+
+		window.row().fill().expandX();
+		window.add(firstOptionButton).align(Align.CENTER).padLeft(20).padRight(20).expandX();
+		window.add(secondOptionButton).align(Align.CENTER).padLeft(20).padRight(20).expandX();
+
+		FlickScrollPane scrollPane = new FlickScrollPane(window);
+
+		scrollPane.width = Gdx.graphics.getWidth() * 0.95f;
+		scrollPane.height = Gdx.graphics.getHeight() * 0.95f;
+		scrollPane.x = Gdx.graphics.getWidth() * 0.5f - scrollPane.width * 0.5f;
+		scrollPane.y = Gdx.graphics.getHeight() * 0.5f - scrollPane.height * 0.5f;
+
+		return scrollPane;
+	}
+
 
 }
