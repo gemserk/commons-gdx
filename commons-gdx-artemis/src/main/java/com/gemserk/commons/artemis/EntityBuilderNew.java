12fce0fd5da804cab0b404d482426efac8683caf
Ariel Coppes <ariel.coppes@gemserk.com>
2012-03-23 11:49:39 -0300

    added new entity builder named EntityBuilderNew (will replace the other) which allows applying a template by using the method template(template) or template(template, parameters)

---
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/EntityBuilderNew.java
@@ -0,0 +1,52 @@
+package com.gemserk.commons.artemis;
+
+import com.artemis.Component;
+import com.artemis.Entity;
+import com.artemis.World;
+import com.gemserk.commons.artemis.templates.EntityTemplate;
+import com.gemserk.componentsengine.utils.Parameters;
+
+public class EntityBuilderNew {
+
+	private final World world;
+
+	private Entity entity;
+
+	public EntityBuilderNew(World world) {
+		this.world = world;
+	}
+
+	public EntityBuilderNew component(Component component) {
+		checkEntityCreated();
+		entity.addComponent(component);
+		return this;
+	}
+
+	public EntityBuilderNew template(EntityTemplate template) {
+		checkEntityCreated();
+		template.apply(entity);
+		return this;
+	}
+
+	public EntityBuilderNew template(EntityTemplate template, Parameters parameters) {
+		checkEntityCreated();
+		template.apply(entity, parameters);
+		return this;
+	}
+
+	private void checkEntityCreated() {
+		if (entity == null)
+			world.createEntity();
+	}
+
+	public Entity build() {
+		checkEntityCreated();
+
+		Entity builtEntity = entity;
+		entity = null;
+
+		builtEntity.refresh();
+		return builtEntity;
+	}
+
+}
