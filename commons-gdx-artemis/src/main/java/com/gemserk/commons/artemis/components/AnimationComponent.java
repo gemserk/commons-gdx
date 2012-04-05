c610c66588a4b3c587e9d79de8ea264a1a0cf2b4
Ariel Coppes <ariel.coppes@gemserk.com>
2012-04-05 08:35:08 -0300

    added static get method to animation component

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/AnimationComponent.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/AnimationComponent.java
@@ -1,9 +1,18 @@
 package com.gemserk.commons.artemis.components;
 
 import com.artemis.Component;
+import com.artemis.ComponentType;
+import com.artemis.ComponentTypeManager;
+import com.artemis.Entity;
 import com.gemserk.animation4j.gdx.Animation;
 
 public class AnimationComponent extends Component {
+
+	public static final ComponentType type = ComponentTypeManager.getTypeFor(AnimationComponent.class);
+
+	public static AnimationComponent get(Entity e) {
+		return (AnimationComponent) e.getComponent(type);
+	}
 
 	private final Animation[] animations;
 
