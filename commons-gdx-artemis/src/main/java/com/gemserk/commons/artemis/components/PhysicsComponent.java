93a216752eb1301147f8f7c2d6243ee00808bae6
Ariel Coppes <ariel.coppes@gemserk.com>
2012-04-05 11:45:50 -0300

    added get method to physics component

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/PhysicsComponent.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/PhysicsComponent.java
@@ -1,12 +1,21 @@
 package com.gemserk.commons.artemis.components;
 
 import com.artemis.Component;
+import com.artemis.ComponentType;
+import com.artemis.ComponentTypeManager;
+import com.artemis.Entity;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.gemserk.commons.gdx.box2d.Contacts;
 import com.gemserk.commons.gdx.games.Physics;
 import com.gemserk.commons.gdx.games.PhysicsImpl;
 
 public class PhysicsComponent extends Component {
+	
+	public static final ComponentType type = ComponentTypeManager.getTypeFor(PhysicsComponent.class);
+
+	public static PhysicsComponent get(Entity e) {
+		return (PhysicsComponent) e.getComponent(type);
+	}
 
 	private final Physics physics;
 	
