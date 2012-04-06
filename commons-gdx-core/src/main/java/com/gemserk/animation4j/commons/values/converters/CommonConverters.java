09e6764251ef0951ad9a8a0d5df17f744edf6e9e
Ariel Coppes <ariel.coppes@gemserk.com>
2012-04-06 11:50:45 -0300

    added Spatial position converter to CommonConverters

--- commons-gdx-core/src/main/java/com/gemserk/animation4j/commons/values/converters/CommonConverters.java
+++ commons-gdx-core/src/main/java/com/gemserk/animation4j/commons/values/converters/CommonConverters.java
@@ -1,6 +1,7 @@
 package com.gemserk.animation4j.commons.values.converters;
 
 import com.gemserk.animation4j.converters.TypeConverter;
+import com.gemserk.commons.gdx.games.Spatial;
 import com.gemserk.commons.values.FloatValue;
 
 public class CommonConverters {
@@ -13,4 +14,27 @@ public class CommonConverters {
 	
 	public static final SpatialSizeTypeConverter spatialSizeTypeConverter = new SpatialSizeTypeConverter();
 
+	public static final TypeConverter<Spatial> spatialPositionConverter = new TypeConverter<Spatial>() {
+
+		@Override
+		public int variables() {
+			return 2;
+		}
+
+		@Override
+		public float[] copyFromObject(Spatial spatial, float[] x) {
+			if (x == null)
+				x = new float[variables()];
+			x[0] = spatial.getX();
+			x[1] = spatial.getY();
+			return x;
+		}
+
+		@Override
+		public Spatial copyToObject(Spatial spatial, float[] x) {
+			spatial.setPosition(x[0], x[1]);
+			return null;
+		}
+		
+	};
 }
