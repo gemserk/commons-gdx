f4b504e41d0bcf096d4dca6dee1b3d11df4bcc26
Ariel Coppes <ariel.coppes@gemserk.com>
2012-04-09 17:29:22 -0300

    added static get to renderable component

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/RenderableComponent.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/components/RenderableComponent.java
@@ -1,8 +1,17 @@
 package com.gemserk.commons.artemis.components;
 
 import com.artemis.Component;
+import com.artemis.ComponentType;
+import com.artemis.ComponentTypeManager;
+import com.artemis.Entity;
 
 public class RenderableComponent extends Component {
+	
+	public static final ComponentType type = ComponentTypeManager.getTypeFor(RenderableComponent.class);
+
+	public static RenderableComponent get(Entity e) {
+		return (RenderableComponent) e.getComponent(type);
+	}
 
 	private boolean visible;
 	private int layer;
---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/monitors/FloatMonitor.java
@@ -0,0 +1,24 @@
+package com.gemserk.commons.monitors;
+
+public class FloatMonitor {
+	
+	float value;
+	boolean changed;
+	
+	float epsilon = 0.001f;
+
+	public FloatMonitor(float value) {
+		this.value = value;
+		this.changed = false;
+	}
+
+	public void update(float newvalue) {
+		changed = (Math.abs(newvalue - value) < epsilon);
+		value = newvalue;
+	}
+
+	public boolean isChanged() {
+		return changed;
+	}
+
+}
