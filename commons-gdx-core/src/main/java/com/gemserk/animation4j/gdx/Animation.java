9b0ed87b72cf015a5ef748060838363373fac745
Ariel Coppes <ariel.coppes@gemserk.com>
2012-04-10 13:24:37 -0300

    changed commons-gdx Animation to implement animatino4j Animation API to have methods like start/pause/etc to be used in some templates

--- commons-gdx-core/src/main/java/com/gemserk/animation4j/gdx/Animation.java
+++ commons-gdx-core/src/main/java/com/gemserk/animation4j/gdx/Animation.java
@@ -3,50 +3,98 @@ package com.gemserk.animation4j.gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.gemserk.animation4j.FrameAnimation;
 
-public class Animation  {
-	
+public class Animation implements com.gemserk.animation4j.animations.Animation {
+
 	private final Sprite[] sprites;
-	
 	private final FrameAnimation frameAnimation;
-	
+
+	boolean playing, started;
+
 	public Animation(Sprite[] frames, FrameAnimation frameAnimation) {
 		this.sprites = frames;
 		this.frameAnimation = frameAnimation;
+		this.playing = true;
+		this.started = true;
 	}
-	
+
 	public int getFramesCount() {
 		return sprites.length;
 	}
-	
+
 	public Sprite getFrame(int index) {
 		return sprites[index];
 	}
-	
+
 	public void setFrame(int index) {
 		frameAnimation.setFrame(index);
 	}
-	
+
 	public int getCurrentFrameIndex() {
 		return frameAnimation.getCurrentFrame();
 	}
-	
+
 	public int getIteration() {
 		return frameAnimation.getIteration();
 	}
-	
+
 	public Sprite getCurrentFrame() {
 		return getFrame(frameAnimation.getCurrentFrame());
 	}
-	
+
 	public void update(float delta) {
+		if (!playing)
+			return;
 		frameAnimation.update(delta);
 	}
-	
+
 	public boolean isFinished() {
 		return frameAnimation.isFinished();
 	}
 
 	public void restart() {
+		playing = true;
 		frameAnimation.restart();
+	}
+
+	@Override
+	public void start() {
+		playing = true;
+		frameAnimation.restart();
+	}
+
+	@Override
+	public void start(int iterationCount) {
+		throw new UnsupportedOperationException("not implemented yet");
+	}
+
+	@Override
+	public void start(int iterationCount, boolean alternateDirection) {
+		throw new UnsupportedOperationException("not implemented yet");
+	}
+
+	@Override
+	public void stop() {
+		playing = false;
+		frameAnimation.restart();
+	}
+
+	@Override
+	public void pause() {
+		playing = false;
+	}
+
+	@Override
+	public void resume() {
+		playing = true;
+	}
+
+	@Override
+	public boolean isStarted() {
+		return started;
+	}
+
+	@Override
+	public PlayingDirection getPlayingDirection() {
+		return PlayingDirection.Normal;
 	}
 }
