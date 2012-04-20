44731af56bee5ed6bb5805d4f8961cf99a61ed33
Ariel Coppes <ariel.coppes@gemserk.com>
2012-04-20 19:53:12 -0300

    added StoreComponent usage to the AliveSystem

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/AliveSystem.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/AliveSystem.java
@@ -3,21 +3,28 @@ package com.gemserk.commons.artemis.systems;
 import com.artemis.Entity;
 import com.artemis.EntityProcessingSystem;
 import com.gemserk.commons.artemis.components.AliveComponent;
+import com.gemserk.commons.artemis.components.StoreComponent;
 import com.gemserk.commons.gdx.GlobalTime;
 
 public class AliveSystem extends EntityProcessingSystem {
 
+	@SuppressWarnings("unchecked")
 	public AliveSystem() {
 		super(AliveComponent.class);
 	}
 
 	@Override
-	protected void process(Entity entity) {
-		AliveComponent aliveComponent = entity.getComponent(AliveComponent.class);
+	protected void process(Entity e) {
+		AliveComponent aliveComponent = e.getComponent(AliveComponent.class);
 		float aliveTime = aliveComponent.getTime() - GlobalTime.getDelta();
 		aliveComponent.setTime(aliveTime);
-		if (aliveTime <= 0) 
-			entity.delete();
+		if (aliveTime <= 0) {
+			StoreComponent storeComponent = StoreComponent.get(e);
+			if (storeComponent != null)
+				storeComponent.store.free(e);
+			else
+				e.delete();
+		}
 	}
 
 }
