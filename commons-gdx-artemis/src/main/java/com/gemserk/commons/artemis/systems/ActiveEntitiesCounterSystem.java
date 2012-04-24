49dd03f28fa02874fd88a49614176d2a3ce2902e
Ruben Garat <ruben.garat@gemserk.com>
2012-04-24 19:59:03 -0300

    added activeentitiescounter system

---
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/ActiveEntitiesCounterSystem.java
@@ -0,0 +1,32 @@
+package com.gemserk.commons.artemis.systems;
+
+import com.artemis.Entity;
+import com.artemis.EntitySystem;
+import com.artemis.utils.ImmutableBag;
+import com.badlogic.gdx.utils.IntMap;
+
+public class ActiveEntitiesCounterSystem extends EntitySystem {
+
+	IntMap<Entity> activeEntities = new IntMap<Entity>();
+
+	@Override
+	protected void processEntities(ImmutableBag<Entity> entities) {
+	}
+
+	@Override
+	protected boolean checkProcessing() {
+		return false;
+	}
+
+	@Override
+	protected void change(Entity e) {
+		if (e.isEnabled())
+			activeEntities.put(e.getId(), null);
+		else
+			activeEntities.remove(e.getId());
+	}
+
+	public int getActiveEntitiesCount() {
+		return activeEntities.size;
+	}
+}
