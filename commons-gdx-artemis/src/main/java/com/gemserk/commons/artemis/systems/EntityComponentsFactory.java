0260e52d4dfda8274527177128772d865a39504b
Ruben Garat <ruben.garat@gemserk.com>
2012-04-26 17:27:21 -0300

    added system to cache the components by entity for use in systems

---
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/EntityComponentsFactory.java
@@ -0,0 +1,35 @@
+package com.gemserk.commons.artemis.systems;
+
+import com.artemis.Entity;
+import com.artemis.utils.Bag;
+import com.badlogic.gdx.utils.IntMap;
+
+public abstract class EntityComponentsFactory<T> {
+	public abstract T newInstance();
+	public abstract void free(T entityComponent);
+	public abstract void load(Entity e, T entityComponent);
+	
+	public void add(Entity entity){
+		T entityComponent;
+		if(pool.isEmpty()){
+			entityComponent = newInstance();
+		} else {
+			entityComponent = pool.removeLast();
+		}
+		load(entity,entityComponent);
+		entityComponents.put(entity.getId(), entityComponent);
+	}
+	
+	public void remove(Entity entity){
+		T entityComponent = entityComponents.remove(entity.getId());
+		free(entityComponent);
+		pool.add(entityComponent);
+	}
+	
+	public T get(Entity entity){
+		return entityComponents.get(entity.getId());
+	}
+	
+	public final IntMap<T> entityComponents = new IntMap<T>();
+	Bag<T> pool = new Bag<T>();
+}
