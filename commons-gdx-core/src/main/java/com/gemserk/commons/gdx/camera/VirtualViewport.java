423f1cf315d6ccaa10d29511ff1dc453818f4095
Ariel Coppes <ariel.coppes@gemserk.com>
2012-04-27 15:08:54 -0300

    added the virtual viewport + its test

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/camera/VirtualViewport.java
@@ -0,0 +1,84 @@
+package com.gemserk.commons.gdx.camera;
+
+import com.badlogic.gdx.Gdx;
+
+public class VirtualViewport {
+
+	float virtualWidth;
+	float virtualHeight;
+
+	boolean shrink;
+
+	public float getVirtualWidth() {
+		return virtualWidth;
+	}
+
+	public float getVirtualHeight() {
+		return virtualHeight;
+	}
+
+	public VirtualViewport(float virtualWidth, float virtualHeight) {
+		this(virtualWidth, virtualHeight, false);
+	}
+
+	public VirtualViewport(float virtualWidth, float virtualHeight, boolean shrink) {
+		this.virtualWidth = virtualWidth;
+		this.virtualHeight = virtualHeight;
+		this.shrink = shrink;
+	}
+
+	public float getWidth() {
+		return getWidth(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	}
+
+	public float getHeight() {
+		return getHeight(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	}
+
+	/**
+	 * Returns the view port width to let all the virtual view port to be shown on the screen.
+	 * 
+	 * @param screenWidth
+	 *            The screen width.
+	 * @param screenHeight
+	 *            The screen Height.
+	 */
+	public float getWidth(float screenWidth, float screenHeight) {
+		float aspect = screenWidth / screenHeight;
+		if (screenWidth > screenHeight) {
+			if (!shrink)
+				return virtualHeight * aspect;
+			return virtualWidth;
+		} else {
+			if (shrink)
+				return virtualWidth * aspect;
+			return virtualWidth;
+		}
+	}
+
+	/**
+	 * Returns the view port height to let all the virtual view port to be shown on the screen.
+	 * 
+	 * @param screenWidth
+	 *            The screen width.
+	 * @param screenHeight
+	 *            The screen Height.
+	 */
+	public float getHeight(float screenWidth, float screenHeight) {
+		float aspect = screenWidth / screenHeight;
+		if (screenWidth > screenHeight) {
+			if (!shrink)
+				return virtualHeight;
+			return virtualWidth / aspect;
+		} else {
+			if (shrink)
+				return virtualWidth;
+			return virtualWidth * aspect;
+		}
+	}
+
+	public void setShrink(boolean inverted) {
+		this.shrink = inverted;
+	}
+	
+}
---
+++ commons-gdx-core/src/test/java/com/gemserk/commons/gdx/camera/VirtualViewportTest.java
@@ -0,0 +1,47 @@
+package com.gemserk.commons.gdx.camera;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+
+import com.gemserk.commons.gdx.camera.VirtualViewport;
+
+public class VirtualViewportTest {
+
+	@Test
+	public void shouldReturnSameSizeIfSameViewport() {
+		VirtualViewport virtualViewport = new VirtualViewport(800, 600);
+		assertThat(virtualViewport.getWidth(800f, 600f), IsEqual.equalTo(800f));
+		assertThat(virtualViewport.getHeight(800f, 600f), IsEqual.equalTo(600f));
+	}
+
+	@Test
+	public void shouldReturnDoubleForMiddleSizeButSameAspect() {
+		VirtualViewport virtualViewport = new VirtualViewport(800, 600);
+		assertThat(virtualViewport.getWidth(400f, 300f), IsEqual.equalTo(800f));
+		assertThat(virtualViewport.getHeight(400f, 300f), IsEqual.equalTo(600f));
+	}
+
+	@Test
+	public void shouldReturnModifiedHeightToLetSameVirtualViewport() {
+		VirtualViewport virtualViewport = new VirtualViewport(800, 600);
+		assertThat(virtualViewport.getWidth(800f, 480f), IsEqual.equalTo(1000f));
+		assertThat(virtualViewport.getHeight(800f, 480f), IsEqual.equalTo(600f));
+	}
+
+	@Test
+	public void testShrinkWithNormalViewport() {
+		VirtualViewport virtualViewport = new VirtualViewport(800, 600, true);
+		assertThat(virtualViewport.getWidth(800f, 600f), IsEqual.equalTo(800f));
+		assertThat(virtualViewport.getHeight(800f, 600f), IsEqual.equalTo(600f));
+	}
+
+	@Test
+	public void testAdaptToInverted() {
+		VirtualViewport virtualViewport = new VirtualViewport(800, 600, true);
+		assertThat(virtualViewport.getWidth(800f, 480f), IsEqual.equalTo(800f));
+		assertThat(virtualViewport.getHeight(800f, 480f), IsEqual.equalTo(480f));
+	}
+	
+}
