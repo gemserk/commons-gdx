6199a8eabcd486d992a9d6812ff881d2d850a5d8
Ariel Coppes <ariel.coppes@gemserk.com>
2012-04-27 16:59:47 -0300

    moved the fields to the top of the EntityComponentsFactory class and ctrl+f

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/EntityComponentsFactory.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/EntityComponentsFactory.java
@@ -5,32 +5,36 @@ import com.artemis.utils.Bag;
 import com.badlogic.gdx.utils.IntMap;
 
 public abstract class EntityComponentsFactory<T> {
+
+	public final IntMap<T> entityComponents = new IntMap<T>();
+	final Bag<T> pool = new Bag<T>();
+
 	public abstract T newInstance();
+
 	public abstract void free(T entityComponent);
+
 	public abstract void load(Entity e, T entityComponent);
-	
-	public T add(Entity entity){
+
+	public T add(Entity entity) {
 		T entityComponent;
-		if(pool.isEmpty()){
+		if (pool.isEmpty()) {
 			entityComponent = newInstance();
 		} else {
 			entityComponent = pool.removeLast();
 		}
-		load(entity,entityComponent);
+		load(entity, entityComponent);
 		entityComponents.put(entity.getId(), entityComponent);
 		return entityComponent;
 	}
-	
-	public void remove(Entity entity){
+
+	public void remove(Entity entity) {
 		T entityComponent = entityComponents.remove(entity.getId());
 		free(entityComponent);
 		pool.add(entityComponent);
 	}
-	
-	public T get(Entity entity){
+
+	public T get(Entity entity) {
 		return entityComponents.get(entity.getId());
 	}
-	
-	public final IntMap<T> entityComponents = new IntMap<T>();
-	Bag<T> pool = new Bag<T>();
+
 }
