e9a135e30ffa7b9c8444e65de3cf534996831dd3
Ruben Garat <ruben.garat@gemserk.com>
2012-04-26 21:28:19 -0300

    alivesystem now uses entitycomponentsfactory

--- commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/AliveSystem.java
+++ commons-gdx-artemis/src/main/java/com/gemserk/commons/artemis/systems/AliveSystem.java
@@ -8,14 +8,43 @@ import com.gemserk.commons.gdx.GlobalTime;
 
 public class AliveSystem extends EntityProcessingSystem {
 
+	class EntityComponents {
+		AliveComponent aliveComponent;
+	}
+	
+	class Factory extends EntityComponentsFactory<EntityComponents>{
+
+		@Override
+		public EntityComponents newInstance() {
+			return new EntityComponents();
+		}
+
+		@Override
+		public void free(EntityComponents entityComponent) {
+			entityComponent.aliveComponent = null;
+			
+		}
+
+		@Override
+		public void load(Entity e, EntityComponents entityComponent) {
+			entityComponent.aliveComponent = AliveComponent.get(e);
+		}
+		
+	}
+
+	private Factory factory;
+	
+	
 	@SuppressWarnings("unchecked")
 	public AliveSystem() {
 		super(AliveComponent.class);
+		this.factory = new Factory();
 	}
 
 	@Override
 	protected void process(Entity e) {
-		AliveComponent aliveComponent = e.getComponent(AliveComponent.class);
+		EntityComponents components = factory.get(e);
+		AliveComponent aliveComponent = components.aliveComponent;
 		float aliveTime = aliveComponent.getTime() - GlobalTime.getDelta();
 		aliveComponent.setTime(aliveTime);
 		if (aliveTime <= 0) {
@@ -25,6 +54,18 @@ public class AliveSystem extends EntityProcessingSystem {
 			else
 				e.delete();
 		}
+	}
+	
+	@Override
+	protected void enabled(Entity e) {
+		super.enabled(e);
+		factory.add(e);
+	}
+	
+	@Override
+	protected void disabled(Entity e) {
+		factory.remove(e);
+		super.disabled(e);
 	}
 
 }
