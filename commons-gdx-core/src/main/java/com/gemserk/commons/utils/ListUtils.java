18f118a04a678a75c131b187544447922b049521
Ariel Coppes <ariel.coppes@gemserk.com>
2012-05-11 19:22:22 -0300

    added utils class for lists with a method to return the next element in a list

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/utils/ListUtils.java
@@ -0,0 +1,17 @@
+package com.gemserk.commons.utils;
+
+import java.util.List;
+
+/**
+ * Provides some useful methdos to be used with lists.
+ */
+public class ListUtils {
+
+	public static <T> T nextElement(List<T> list, T element) {
+		int index = list.indexOf(element);
+		if (index == -1)
+			throw new IllegalArgumentException("Can't get next element of an element not in the list");
+		index = (index + 1) % list.size();
+		return list.get(index);
+	}
+}
