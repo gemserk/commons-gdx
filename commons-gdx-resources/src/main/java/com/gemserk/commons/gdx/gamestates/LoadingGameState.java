673bfb5c2a33db4ebaf08cfdf5668aaeb356e06f
Ariel Coppes <ariel.coppes@gemserk.com>
2012-05-11 10:40:36 -0300

    modified the LoadingGameState a bit to try to load more than one task in the same render call if the loading time was less than 30ms to not limit < 30ms tasks load to frame rate

--- commons-gdx-resources/src/main/java/com/gemserk/commons/gdx/gamestates/LoadingGameState.java
+++ commons-gdx-resources/src/main/java/com/gemserk/commons/gdx/gamestates/LoadingGameState.java
@@ -6,7 +6,12 @@ import com.gemserk.resources.progress.TaskQueue;
 public class LoadingGameState extends GameStateImpl {
 
 	private TaskQueue taskQueue;
+
+	// 30ms in ns...
+	private final long loadTimePerFrame = 30L * 1000000L;
 	
+	private long loadTime;
+
 	public TaskQueue getTaskQueue() {
 		return taskQueue;
 	}
@@ -20,9 +25,19 @@ public class LoadingGameState extends GameStateImpl {
 	@Override
 	public void render() {
 		super.render();
-		// should be on the update if the update was processed once 
-		if (!taskQueue.isDone()) 
+		// should be on the update if the update was processed once
+		long lastDelta = System.nanoTime();
+		long currentDelta = lastDelta;
+		
+		loadTime = 0;
+		
+		// this allows to perform more than one task in the same render call 
+		while (!taskQueue.isDone() && loadTime <= loadTimePerFrame) {
 			taskQueue.processNext();
+			currentDelta = System.nanoTime();
+			loadTime += currentDelta - lastDelta;
+		}
+		
 	}
-	
+
 }
