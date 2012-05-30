afbf3e344abc585bbcd524cd79c84176c5459b46
Ariel Coppes <ariel.coppes@gemserk.com>
2012-05-30 18:08:56 -0300

    just formatted multiple virtual view stuff

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/camera/MultipleVirtualViewportBuilder.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/camera/MultipleVirtualViewportBuilder.java
@@ -18,37 +18,37 @@ public class MultipleVirtualViewportBuilder {
 		if (width >= minWidth && width <= maxWidth && height >= minHeight && height <= maxHeight)
 			return new VirtualViewport(width, height, true);
 
-//		float aspect = width / height;
-//
-//		float scaleForMinSize = minHeight / height;
-//		float scaleForMaxSize = maxHeight / height;
-//
-//		float virtualViewportHeight = height * scaleForMaxSize;
-//		float virtualViewportWidth = virtualViewportHeight * aspect;
-//
-//		if (virtualViewportWidth >= minWidth && virtualViewportWidth <= maxWidth)
-//			return new VirtualViewport(virtualViewportWidth, virtualViewportHeight, true);
-//
-//		virtualViewportHeight = height * scaleForMinSize;
-//		virtualViewportWidth = virtualViewportHeight * aspect;
-//
-//		return new VirtualViewport(virtualViewportWidth, virtualViewportHeight, true);
+		// float aspect = width / height;
+		//
+		// float scaleForMinSize = minHeight / height;
+		// float scaleForMaxSize = maxHeight / height;
+		//
+		// float virtualViewportHeight = height * scaleForMaxSize;
+		// float virtualViewportWidth = virtualViewportHeight * aspect;
+		//
+		// if (virtualViewportWidth >= minWidth && virtualViewportWidth <= maxWidth)
+		// return new VirtualViewport(virtualViewportWidth, virtualViewportHeight, true);
+		//
+		// virtualViewportHeight = height * scaleForMinSize;
+		// virtualViewportWidth = virtualViewportHeight * aspect;
+		//
+		// return new VirtualViewport(virtualViewportWidth, virtualViewportHeight, true);
 
-		 float aspect = width / height;
-		
-		 float scaleForMinSize = minWidth / width;
-		 float scaleForMaxSize = maxWidth / width;
-		
-		 float virtualViewportWidth = width * scaleForMaxSize;
-		 float virtualViewportHeight = virtualViewportWidth / aspect;
-		
-		 if (virtualViewportHeight >= minHeight && virtualViewportHeight <= maxHeight)
-		 return new VirtualViewport(virtualViewportWidth, virtualViewportHeight, true);
-		
-		 virtualViewportWidth = width * scaleForMinSize;
-		 virtualViewportHeight = virtualViewportWidth / aspect;
-		
-		 return new VirtualViewport(virtualViewportWidth, virtualViewportHeight, true);
+		float aspect = width / height;
+
+		float scaleForMinSize = minWidth / width;
+		float scaleForMaxSize = maxWidth / width;
+
+		float virtualViewportWidth = width * scaleForMaxSize;
+		float virtualViewportHeight = virtualViewportWidth / aspect;
+
+		if (virtualViewportHeight >= minHeight && virtualViewportHeight <= maxHeight)
+			return new VirtualViewport(virtualViewportWidth, virtualViewportHeight, true);
+
+		virtualViewportWidth = width * scaleForMinSize;
+		virtualViewportHeight = virtualViewportWidth / aspect;
+
+		return new VirtualViewport(virtualViewportWidth, virtualViewportHeight, true);
 	}
 
 }
