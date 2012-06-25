de4a415ffbbb7d41d97f34daf4a25438d4c76cc7
Ariel Coppes <ariel.coppes@gemserk.com>
2012-06-25 19:22:53 -0300

    just removde unused imports

--- commons-gdx-android/src/main/java/com/gemserk/commons/admob/AdMobHandler.java
+++ commons-gdx-android/src/main/java/com/gemserk/commons/admob/AdMobHandler.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.LinearInterpolator;
-import android.view.animation.TranslateAnimation;
 import android.widget.RelativeLayout;
 
 import com.google.ads.AdView;
