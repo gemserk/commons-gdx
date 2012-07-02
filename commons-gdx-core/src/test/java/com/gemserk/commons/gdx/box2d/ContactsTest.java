7060e9d22744bbdf39a3a8a1bdfc171f6a834c8c
Ruben Garat <ruben.garat@gemserk.com>
2012-07-02 14:53:40 -0300

    fixed contacts test

--- commons-gdx-core/src/test/java/com/gemserk/commons/gdx/box2d/ContactsTest.java
+++ commons-gdx-core/src/test/java/com/gemserk/commons/gdx/box2d/ContactsTest.java
@@ -36,166 +36,197 @@ public class ContactsTest {
 	private Vector2 normal;
 	private Fixture fixtureA;
 	private Fixture fixtureB;
-	
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		contacts = new Contacts();
 		box2dcontact = mockery.mock(com.badlogic.gdx.physics.box2d.Contact.class);
 		worldManifold = mockery.mock(WorldManifold.class);
-		normal = new Vector2(0,1);
-		fixtureA = mockery.mock(Fixture.class,"fixtureA");
-		fixtureB = mockery.mock(Fixture.class,"fixtureB");
+		normal = new Vector2(0, 1);
+		fixtureA = mockery.mock(Fixture.class, "fixtureA");
+		fixtureB = mockery.mock(Fixture.class, "fixtureB");
 	}
-	
+
 	@Test
-	public void onCreationThereAreNoActiveContacts(){
-		
+	public void onCreationThereAreNoActiveContacts() {
+
 		assertThat(0, IsEqual.equalTo(contacts.getContactCount()));
 		assertFalse(contacts.isInContact());
 	}
-	
+
 	@Test
-	public void whenAddingOneContactABItIsAdded(){
-		
+	public void whenAddingOneContactABItIsAdded() {
+
 		mockery.checking(new Expectations() {
 			{
-				oneOf(box2dcontact).getWorldManifold();will(returnValue(worldManifold));
-				oneOf(worldManifold).getNormal();will(returnValue(normal));
-				oneOf(box2dcontact).getFixtureA();will(returnValue(fixtureA));
-				oneOf(box2dcontact).getFixtureB();will(returnValue(fixtureB));
+				oneOf(box2dcontact).getWorldManifold();
+				will(returnValue(worldManifold));
+				oneOf(worldManifold).getNormal();
+				will(returnValue(normal));
+				oneOf(box2dcontact).getFixtureA();
+				will(returnValue(fixtureA));
+				oneOf(box2dcontact).getFixtureB();
+				will(returnValue(fixtureB));
+				ignoring(fixtureA);
+				ignoring(fixtureB);
 			}
 		});
-		
+
 		contacts.addContact(box2dcontact, true);
 		assertTrue(contacts.isInContact());
 		assertThat(1, IsEqual.equalTo(contacts.getContactCount()));
-		
-		
+
 		Contact contact = contacts.getContact(0);
-		assertThat(contact.getNormal().dst(0,1), OrderingComparisons.lessThan(0.1f));
+		assertThat(contact.getNormal().dst(0, 1), OrderingComparisons.lessThan(0.1f));
 		assertSame(fixtureA, contact.getMyFixture());
 		assertSame(fixtureB, contact.getOtherFixture());
-		
+
 	}
-	
+
 	@Test
-	public void whenAddingOneContactBAItIsAdded(){
-		
+	public void whenAddingOneContactBAItIsAdded() {
+
 		mockery.checking(new Expectations() {
 			{
-				oneOf(box2dcontact).getWorldManifold();will(returnValue(worldManifold));
-				oneOf(worldManifold).getNormal();will(returnValue(normal));
-				oneOf(box2dcontact).getFixtureA();will(returnValue(fixtureA));
-				oneOf(box2dcontact).getFixtureB();will(returnValue(fixtureB));
+				oneOf(box2dcontact).getWorldManifold();
+				will(returnValue(worldManifold));
+				oneOf(worldManifold).getNormal();
+				will(returnValue(normal));
+				oneOf(box2dcontact).getFixtureA();
+				will(returnValue(fixtureA));
+				oneOf(box2dcontact).getFixtureB();
+				will(returnValue(fixtureB));
+				ignoring(fixtureA);
+				ignoring(fixtureB);
 			}
 		});
-		
+
 		contacts.addContact(box2dcontact, false);
 		assertTrue(contacts.isInContact());
 		assertThat(1, IsEqual.equalTo(contacts.getContactCount()));
-		
-		
+
 		Contact contact = contacts.getContact(0);
-		assertThat(contact.getNormal().dst(0,-1), OrderingComparisons.lessThan(0.1f));
+		assertThat(contact.getNormal().dst(0, -1), OrderingComparisons.lessThan(0.1f));
 		assertSame(fixtureA, contact.getOtherFixture());
 		assertSame(fixtureB, contact.getMyFixture());
-		
+
 	}
-	
+
 	@Test
-	public void removesFirstContactIfItMatches(){
-		
+	public void removesFirstContactIfItMatches() {
+
+		mockery.checking(new Expectations() {
+			{
+				ignoring(fixtureA);
+				ignoring(fixtureB);
+			}
+		});
+
 		contacts.addContact(fixtureA, fixtureB, normal);
-		
+
 		assertTrue(contacts.isInContact());
 		assertThat(1, IsEqual.equalTo(contacts.getContactCount()));
-		
-		
-		
+
 		contacts.removeContact(fixtureA, fixtureB);
 		assertFalse(contacts.isInContact());
 		assertThat(0, IsEqual.equalTo(contacts.getContactCount()));
-		
+
 		Contact removedContact = contacts.contacts.get(0);
 		assertFalse(removedContact.inContact);
 		assertNull(removedContact.myFixture);
 		assertNull(removedContact.otherFixture);
 	}
-	
+
 	@Test
-	public void removesSecondContactIfItMatches(){
-		
-		Fixture fixtureC = mockery.mock(Fixture.class);
+	public void removesSecondContactIfItMatches() {
+
+		final Fixture fixtureC = mockery.mock(Fixture.class);
+
+		mockery.checking(new Expectations() {
+			{
+				ignoring(fixtureA);
+				ignoring(fixtureB);
+				ignoring(fixtureC);
+			}
+		});
+
 		contacts.addContact(fixtureA, fixtureC, new Vector2());
 		contacts.addContact(fixtureA, fixtureB, normal);
-		
+
 		assertTrue(contacts.isInContact());
 		assertThat(2, IsEqual.equalTo(contacts.getContactCount()));
-		
-		
-		
+
 		contacts.removeContact(fixtureA, fixtureB);
 		assertTrue(contacts.isInContact());
 		assertThat(1, IsEqual.equalTo(contacts.getContactCount()));
-		
-		
-		
+
 		Contact stillValidContact = contacts.contacts.get(0);
 		assertTrue(stillValidContact.inContact);
-		assertSame(fixtureA,stillValidContact.myFixture);
-		assertSame(fixtureC,stillValidContact.otherFixture);
-		
+		assertSame(fixtureA, stillValidContact.myFixture);
+		assertSame(fixtureC, stillValidContact.otherFixture);
+
 		Contact removedContact = contacts.contacts.get(1);
 		assertFalse(removedContact.inContact);
 		assertNull(removedContact.myFixture);
 		assertNull(removedContact.otherFixture);
 	}
-	
+
 	@Test
-	public void removesFirstOfTwoContactIfItMatches(){
-		
-		Fixture fixtureC = mockery.mock(Fixture.class);
+	public void removesFirstOfTwoContactIfItMatches() {
+
+		final Fixture fixtureC = mockery.mock(Fixture.class);
+
+		mockery.checking(new Expectations() {
+			{
+				ignoring(fixtureA);
+				ignoring(fixtureB);
+				ignoring(fixtureC);
+			}
+		});
+
 		contacts.addContact(fixtureA, fixtureB, normal);
 		contacts.addContact(fixtureA, fixtureC, new Vector2());
-		
+
 		assertTrue(contacts.isInContact());
 		assertThat(2, IsEqual.equalTo(contacts.getContactCount()));
-		
-		
-		
+
 		contacts.removeContact(fixtureA, fixtureB);
 		assertTrue(contacts.isInContact());
 		assertThat(1, IsEqual.equalTo(contacts.getContactCount()));
-		
-		
-		
+
 		Contact stillValidContact = contacts.contacts.get(0);
 		assertTrue(stillValidContact.inContact);
-		assertSame(fixtureA,stillValidContact.myFixture);
-		assertSame(fixtureC,stillValidContact.otherFixture);
-		
+		assertSame(fixtureA, stillValidContact.myFixture);
+		assertSame(fixtureC, stillValidContact.otherFixture);
+
 		Contact removedContact = contacts.contacts.get(1);
 		assertFalse(removedContact.inContact);
 		assertNull(removedContact.myFixture);
 		assertNull(removedContact.otherFixture);
 	}
-	
+
 	@Test
-	public void addingAContactReusesOneIfAvailable(){
-		
+	public void addingAContactReusesOneIfAvailable() {
+
+		mockery.checking(new Expectations() {
+			{
+				ignoring(fixtureA);
+				ignoring(fixtureB);
+			}
+		});
+
 		Contact reusedContact = new Contact();
 		contacts.contacts.add(reusedContact);
 		contacts.activeContacts = 0;
 		contacts.addContact(fixtureA, fixtureB, new Vector2());
-		
+
 		assertTrue(contacts.isInContact());
 		assertThat(1, IsEqual.equalTo(contacts.getContactCount()));
-				
-		
+
 		Contact contact = contacts.contacts.get(0);
-		assertThat(true,IsEqual.equalTo(contact.inContact));
-		assertThat(contact.myFixture,same(fixtureA));
-		assertThat(contact.otherFixture,same(fixtureB));
+		assertThat(true, IsEqual.equalTo(contact.inContact));
+		assertThat(contact.myFixture, same(fixtureA));
+		assertThat(contact.otherFixture, same(fixtureB));
 	}
-	
+
 }
