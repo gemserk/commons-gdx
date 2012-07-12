41f13013efb3810ca1833f6e5c26e0d1b90541cf
Ariel Coppes <ariel.coppes@gemserk.com>
2012-07-12 12:33:32 -0300

    just added a small comment to the SpriteRegion javadocs

--- commons-gdx-core/src/main/java/com/gemserk/commons/gdx/graphics/SpriteRegion.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/gdx/graphics/SpriteRegion.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
  * Sprite region is a helper class to hold some values to be used when modifying a sprite texture region. 
  * It holds a copy of the original texture region and four values specifying a inner region of the Sprite in relative values. 
  * For example, if you want the full Sprite region then (0, 0, 1, 1) should be used as the inner region values, if you want 
- * the first quarter of the Sprite then (0, 0, 0.5f, 0.5f) should be used.
+ * the first quarter of the Sprite then (0, 0, 0.5f, 0.5f) should be used. 
+ * 
+ * Note: Doesn't support AtlasSprite yet.
  * 
  * @author acoppes
  * 
