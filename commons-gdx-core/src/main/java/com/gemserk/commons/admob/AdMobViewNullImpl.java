51beb0d48fc07c478cbda9e4defb28a92acf5f05
Ariel Coppes <ariel.coppes@gemserk.com>
2012-07-13 16:53:33 -0300

    added a null implementation of admobview

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/admob/AdMobViewNullImpl.java
@@ -0,0 +1,30 @@
+package com.gemserk.commons.admob;
+
+public class AdMobViewNullImpl implements AdMobView {
+
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void show(AdsParameters adsParameters) {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return false;
+	}
+
+	@Override
+	public void setEnabled(boolean enabled) {
+		
+	}
+
+}
--- commons-gdx-resources/src/main/java/com/gemserk/commons/gdx/resources/LibgdxResourceBuilder.java
+++ commons-gdx-resources/src/main/java/com/gemserk/commons/gdx/resources/LibgdxResourceBuilder.java
@@ -3,8 +3,6 @@ package com.gemserk.commons.gdx.resources;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 
 import com.badlogic.gdx.Gdx;
