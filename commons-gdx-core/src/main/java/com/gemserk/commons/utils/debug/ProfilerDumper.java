12cca351e001baf5a983ea49ce54df15667c27b8
Ruben Garat <ruben.garat@gemserk.com>
2012-07-22 20:18:52 -0300

    added class to create method trace profile dump

---
+++ commons-gdx-core/src/main/java/com/gemserk/commons/utils/debug/ProfilerDumper.java
@@ -0,0 +1,17 @@
+package com.gemserk.commons.utils.debug;
+
+public class ProfilerDumper {
+
+	public void startMethodTracing(String name) {
+
+	}
+
+	public void startMethodTracing(String name, int bufferSize) {
+
+	}
+
+	public void stopMethodTracing() {
+
+	}
+
+}
