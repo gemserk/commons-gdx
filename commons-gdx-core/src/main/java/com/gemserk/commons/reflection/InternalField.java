c3d50dbcf0b86edf80f959a840165b94c6b3cdcc
Ariel Coppes <ariel.coppes@gemserk.com>
2012-07-22 20:29:18 -0300

    added a bit of javadocs to the internal field stuff

--- commons-gdx-core/src/main/java/com/gemserk/commons/reflection/InternalField.java
+++ commons-gdx-core/src/main/java/com/gemserk/commons/reflection/InternalField.java
@@ -1,11 +1,31 @@
 package com.gemserk.commons.reflection;
 
+/**
+ * Provides an abstraction to access a field of a class.
+ */
 public interface InternalField {
-	
+
+	/**
+	 * Returns the field name.
+	 */
 	String getFieldName();
-	
+
+	/**
+	 * Returns the value of the field given an instance.
+	 * 
+	 * @param instance
+	 *            The instance to get the value from.
+	 */
 	Object getValue(Object instance);
 
+	/**
+	 * Sets the value of the field given an instance.
+	 * 
+	 * @param instance
+	 *            The instance to set the value to.
+	 * @param value
+	 *            The value to set.
+	 */
 	void setValue(Object instance, Object value);
 
 }
