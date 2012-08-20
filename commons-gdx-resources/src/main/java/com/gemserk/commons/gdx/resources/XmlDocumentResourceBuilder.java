31e0e22e0d817a218c8a1ca9f5ded1a8b21f1202
Ariel Coppes <ariel.coppes@gemserk.com>
2012-08-20 15:09:57 -0300

    added an xml schema resource builder in case we want to specify one, modified the xml document resource builder to recieve the resource manager and the schema resource id optional

--- commons-gdx-resources/src/main/java/com/gemserk/commons/gdx/resources/XmlDocumentResourceBuilder.java
+++ commons-gdx-resources/src/main/java/com/gemserk/commons/gdx/resources/XmlDocumentResourceBuilder.java
@@ -1,15 +1,21 @@
 package com.gemserk.commons.gdx.resources;
 
+import javax.xml.validation.Schema;
+
 import org.w3c.dom.Document;
 
 import com.badlogic.gdx.files.FileHandle;
 import com.gemserk.commons.svg.inkscape.DocumentParser;
+import com.gemserk.resources.ResourceManager;
 
 public class XmlDocumentResourceBuilder implements ResourceBuilder<Document> {
+
+	private final ResourceManager<String> resourceManager;
 
 	private boolean cached = true;
 
 	FileHandle file;
+	String schemaResourceId;
 
 	public XmlDocumentResourceBuilder file(FileHandle file) {
 		this.file = file;
@@ -25,10 +31,23 @@ public class XmlDocumentResourceBuilder implements ResourceBuilder<Document> {
 		return this;
 	}
 
+	public XmlDocumentResourceBuilder schemaResourceId(String schemaResourceId) {
+		this.schemaResourceId = schemaResourceId;
+		return this;
+	}
+
+	public XmlDocumentResourceBuilder(ResourceManager<String> resourceManager) {
+		this.resourceManager = resourceManager;
+	}
+
 	@Override
 	public Document build() {
 		if (file == null)
 			throw new IllegalStateException("Can't create an XML Document if no file handle is specified");
+		if (schemaResourceId != null) {
+			Schema schema = resourceManager.getResourceValue(schemaResourceId);
+			return new DocumentParser().parse(schema, file.read(), false, true);
+		}
 		return new DocumentParser().parse(file.read());
 	}
 
