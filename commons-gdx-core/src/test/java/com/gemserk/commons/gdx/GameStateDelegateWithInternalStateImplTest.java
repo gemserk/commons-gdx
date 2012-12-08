37cdca4c0d1ddcf234c181329b4da94c641bc041
Ariel Coppes <ariel.coppes@gemserk.com>
2012-12-07 23:05:40 -0200

    just added another test for the case of being inited and disposed, render should not be called again

--- commons-gdx-core/src/test/java/com/gemserk/commons/gdx/GameStateDelegateWithInternalStateImplTest.java
+++ commons-gdx-core/src/test/java/com/gemserk/commons/gdx/GameStateDelegateWithInternalStateImplTest.java
@@ -1,6 +1,7 @@
 package com.gemserk.commons.gdx;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -13,6 +14,24 @@ public class GameStateDelegateWithInternalStateImplTest {
 		
 		GameStateDelegateWithInternalStateImpl gameState = new GameStateDelegateWithInternalStateImpl(mockGameState);
 		gameState.show();
+		gameState.render();
+		
+		assertFalse(mockGameState.renderCalled);
+	}
+	
+	@Test
+	public void shouldNotCallRenderIfDisposed() {
+		MockGameState mockGameState = new MockGameState();
+		mockGameState.initialized = false;
+		
+		GameStateDelegateWithInternalStateImpl gameState = new GameStateDelegateWithInternalStateImpl(mockGameState);
+		gameState.init();
+		gameState.show();
+		gameState.render();
+		assertTrue(mockGameState.renderCalled);
+		
+		mockGameState.renderCalled = false;
+		gameState.dispose();
 		gameState.render();
 		
 		assertFalse(mockGameState.renderCalled);
